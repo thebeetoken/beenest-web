6171a44900c05d6a8b2b7a687ec304a7d045e3e5
Jeremy Kwa <jeremykwa@gmail.com>
2019-01-07 15:02:40 -0800

    add error message styles

--- src/components/routes/Host/ListingForm/AccommodationsForm/AccommodationsForm.tsx
+++ src/components/routes/Host/ListingForm/AccommodationsForm/AccommodationsForm.tsx
@@ -4,8 +4,9 @@ import InputLabel from 'shared/InputLabel';
 import InputWrapper from 'shared/InputWrapper';
 import Textarea from 'shared/Textarea';
 import { Field, ErrorMessage } from 'formik';
-import { TextareaEvent } from 'components/shared/Textarea/Textarea';
+import { TextareaEvent } from 'shared/Textarea/Textarea';
 import { stringToArray, arrayToString } from 'utils/formatter';
+import ErrorMessageWrapper from 'shared/ErrorMessageWrapper/ErrorMessageWrapper';
 
 const AccommodationsForm = (props: any): JSX.Element => {
   const { setFieldTouched, setFieldValue, values } = props;
@@ -30,7 +31,7 @@ const AccommodationsForm = (props: any): JSX.Element => {
         </SelectBoxWrapper>
       </div> */}
 
-      <div className="form-item row-bed-bathroom">
+      <div className="form-item row">
         <div className="input-container">
           <InputLabel htmlFor="numberOfBedrooms">Bedrooms</InputLabel>
           <InputWrapper>
@@ -39,7 +40,9 @@ const AccommodationsForm = (props: any): JSX.Element => {
               placeholder="# of Bedrooms"
               type="number" />
           </InputWrapper>
-          <ErrorMessage name="numberOfBedrooms" />
+          <ErrorMessageWrapper>
+            <ErrorMessage name="numberOfBedrooms" />
+          </ErrorMessageWrapper>
         </div>
 
         <div className="input-container">
@@ -53,7 +56,7 @@ const AccommodationsForm = (props: any): JSX.Element => {
         </div>
       </div>
 
-      <div className="form-item row-bed-bathroom">
+      <div className="form-item row">
         <div className="input-container">
           <InputLabel htmlFor="numberOfBathrooms">Bathrooms</InputLabel>
           <InputWrapper>
@@ -63,7 +66,9 @@ const AccommodationsForm = (props: any): JSX.Element => {
               step={0.5}
               type="number" />
           </InputWrapper>
-          <ErrorMessage name="numberOfBathrooms" />
+          <ErrorMessageWrapper>
+            <ErrorMessage name="numberOfBathrooms" />
+          </ErrorMessageWrapper>
         </div>
         
         <div className="input-container">
--- src/components/routes/Host/ListingForm/PricingAvailabilityForm/PricingAvailabilityForm.tsx
+++ src/components/routes/Host/ListingForm/PricingAvailabilityForm/PricingAvailabilityForm.tsx
@@ -3,56 +3,69 @@ import * as React from 'react';
 import InputLabel from 'shared/InputLabel';
 import InputWrapper from 'shared/InputWrapper';
 import Textarea from 'shared/Textarea';
-import { TextareaEvent } from 'components/shared/Textarea/Textarea';
+import { TextareaEvent } from 'shared/Textarea/Textarea';
 import { Field, ErrorMessage } from 'formik';
 import { stringToArray, arrayToString } from 'utils/formatter';
+import ErrorMessageWrapper from 'shared/ErrorMessageWrapper/ErrorMessageWrapper';
 
 const PricingAvailabilityForm = (props: any): JSX.Element => {
   const { setFieldTouched, setFieldValue, values } = props;
   return (
     <>
-      <div className="form-item max-guests">
-        <InputLabel htmlFor="maxGuests">Max Guests</InputLabel>
-        <InputWrapper>
-          <Field
-            name="maxGuests"
-            placeholder="# of Guests"
-            type="number" />
-        </InputWrapper>
-        <ErrorMessage name="maxGuests" />
+      <div className="form-item row">
+        <div className="input-container max-guests">
+          <InputLabel htmlFor="maxGuests">Max Guests</InputLabel>
+          <InputWrapper>
+            <Field
+              name="maxGuests"
+              placeholder="# of Guests"
+              type="number" />
+          </InputWrapper>
+          <ErrorMessageWrapper>
+            <ErrorMessage name="maxGuests" />
+          </ErrorMessageWrapper>
+        </div>
+
+        <div className="input-container min-nights">
+          <InputLabel htmlFor="minimumNights">Min Nights</InputLabel>
+          <InputWrapper>
+            <Field
+              name="minimumNights"
+              placeholder="# of nights"
+              type="number" />
+          </InputWrapper>
+          <ErrorMessageWrapper>
+            <ErrorMessage name="minimumNights" />
+          </ErrorMessageWrapper>
+        </div>
       </div>
 
-      <div className="form-item min-nights">
-        <InputLabel htmlFor="minimumNights">Min Nights</InputLabel>
-        <InputWrapper>
-          <Field
-            name="minimumNights"
-            placeholder="# of nights"
-            type="number" />
-        </InputWrapper>
-        <ErrorMessage name="minimumNights" />
-      </div>
+      <div className="form-item row">
+        <div className="input-container price-per-night">
+          <InputLabel htmlFor="pricePerNightUsd" subLabel="(USD)">Price Per Night</InputLabel>
+          <InputWrapper>
+            <Field
+              name="pricePerNightUsd"
+              placeholder="$"
+              type="number" />
+          </InputWrapper>
+          <ErrorMessageWrapper>
+            <ErrorMessage name="pricePerNightUsd" />
+          </ErrorMessageWrapper>
+        </div>
 
-      <div className="form-item price-per-night">
-        <InputLabel htmlFor="pricePerNightUsd" subLabel="(USD)">Price Per Night</InputLabel>
-        <InputWrapper>
-          <Field
-            name="pricePerNightUsd"
-            placeholder="$"
-            type="number" />
-        </InputWrapper>
-        <ErrorMessage name="pricePerNightUsd" />
-      </div>
-
-      <div className="form-item security-deposit">
-        <InputLabel htmlFor="securityDepositUsd" subLabel="(USD)">Security Deposit</InputLabel>
-        <InputWrapper>
-          <Field
-            name="securityDepositUsd"
-            placeholder="$"
-            type="number" />
-        </InputWrapper>
-        <ErrorMessage name="securityDepositUsd" />
+        <div className="input-container security-deposit">
+          <InputLabel htmlFor="securityDepositUsd" subLabel="(USD)">Security Deposit</InputLabel>
+          <InputWrapper>
+            <Field
+              name="securityDepositUsd"
+              placeholder="$"
+              type="number" />
+          </InputWrapper>
+          <ErrorMessageWrapper>
+            <ErrorMessage name="securityDepositUsd" />
+          </ErrorMessageWrapper>
+        </div>
       </div>
 
       <div className="form-item">
