8850a8f230484f2f79014c751a2f9a7df34b3cd8
Jeremy Kwa <jeremykwa@gmail.com>
2019-01-09 11:18:29 -0800

    remove zeroing the host-cta-image and instead increase calendar z-index

--- src/components/routes/Home/Home.container.ts
+++ src/components/routes/Home/Home.container.ts
@@ -64,7 +64,6 @@ const HomeContainerMobile = styled.section`
         flex-shrink: 0;
         min-height: 162px;
         position: relative;
-        z-index: 0;
       }
       .host-cta-content {
         display: flex;
