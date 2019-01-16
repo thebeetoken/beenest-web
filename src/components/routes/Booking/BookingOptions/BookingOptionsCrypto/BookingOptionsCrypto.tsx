3e2efe9257b5154257e48e250026a0e0d36de5fa
Vic Woeltjen <vic@thebeetoken.com>
2019-01-15 16:01:26 -0800

    Begin refactoring asynchrony-dependent parts

--- src/components/routes/Booking/BookingOptions/BookingOptionsCrypto/BookingOptionsCrypto.tsx
+++ src/components/routes/Booking/BookingOptions/BookingOptionsCrypto/BookingOptionsCrypto.tsx
@@ -11,7 +11,7 @@ import SelectPaymentButton from '../SelectPaymentButton';
 import Button from 'shared/Button';
 import BeeLink from 'shared/BeeLink';
 
-import { Web3Data, isNetworkValid, getValidNetworkName } from 'utils/web3';
+import { balanceOf, Web3Data, isNetworkValid, getValidNetworkName, loadWeb3 } from 'utils/web3';
 import { numberToLocaleString } from 'utils/numberToLocaleString';
 import { AppConsumer, AppConsumerProps, ScreenType } from 'components/App.context';
 
@@ -58,82 +58,86 @@ const BookingOptionsCrypto = ({ booking, currency, fromBee, history }: Props) =>
         if (!quote) {
           return null;
         }
-        const availableFunds = getAvailableAmount(accounts, currency);
-        const total = fromBee ? fromBee(quote.guestTotalAmount) : quote.guestTotalAmount;
-        const hasInsufficientFunds = availableFunds < total;
-        return (
-          <BookingOptionsCryptoContainer>
-            <AppConsumer>
-              {({ screenType }: AppConsumerProps) =>
-                screenType < ScreenType.DESKTOP && (
-                  <div className="mobile-wallet-note">
-                    <p>Note: This is the current wallet selected on your app</p>
-                  </div>
-                )
-              }
-            </AppConsumer>
-            {accounts &&
-              accounts.map(account => (
-                <div className="crypto-container" key={account.walletAddress}>
-                  <div className="crypto-currency">
-                    <h3>Available {currency}:</h3>
-                    <span>{numberToLocaleString(availableFunds, currency)}</span>
+        const renderContents = async () => {
+          const availableFunds = await getAvailableAmount(accounts, currency);
+          const total = fromBee ? fromBee(quote.guestTotalAmount) : quote.guestTotalAmount;
+          const hasInsufficientFunds = availableFunds < total;
+          return (
+            <BookingOptionsCryptoContainer>
+              <AppConsumer>
+                {({ screenType }: AppConsumerProps) =>
+                  screenType < ScreenType.DESKTOP && (
+                    <div className="mobile-wallet-note">
+                      <p>Note: This is the current wallet selected on your app</p>
+                    </div>
+                  )
+                }
+              </AppConsumer>
+              {accounts &&
+                accounts.map(account => (
+                  <div className="crypto-container" key={account.walletAddress}>
+                    <div className="crypto-currency">
+                      <h3>Available {currency}:</h3>
+                      <span>{numberToLocaleString(availableFunds, currency)}</span>
 
-                    {hasInsufficientFunds && (
-                      <div className="booking-options-error">
-                        <p>
-                          You do not have enough tokens for this booking.
-                        </p>
+                      {hasInsufficientFunds && (
+                        <div className="booking-options-error">
+                          <p>
+                            You do not have enough tokens for this booking.
+                          </p>
+                        </div>
+                      )}
+                    </div>
+                    <div className="crypto-address">
+                      <h3>Address:</h3>
+                      <span>{account.walletAddress}</span>
+                    </div>
+                  </div>
+                ))}
+              <AppConsumer>
+                {({ screenType }: AppConsumerProps) => {
+                  if (screenType < ScreenType.TABLET) {
+                    return (
+                      <div className="booking-options-crypto-bar">
+                        <BookingOptionsBar booking={booking} currency={currency} disabled={hasInsufficientFunds} />
                       </div>
-                    )}
-                  </div>
-                  <div className="crypto-address">
-                    <h3>Address:</h3>
-                    <span>{account.walletAddress}</span>
-                  </div>
-                </div>
-              ))}
-            <AppConsumer>
-              {({ screenType }: AppConsumerProps) => {
-                if (screenType < ScreenType.TABLET) {
+                    );
+                  }
                   return (
-                    <div className="booking-options-crypto-bar">
-                      <BookingOptionsBar booking={booking} currency={currency} disabled={hasInsufficientFunds} />
+                    <div>
+                      {accounts && (
+                        <div className="booking-options-disclaimer">
+                          <p>
+                            <span>Note:&nbsp;</span>
+                            Wallet Address is automatically updated to the current wallet selected in your Metamask
+                            browser application. Make sure the correct wallet is selected and matches the wallet address
+                            below.
+                          </p>
+                        </div>
+                      )}
+                      <div className="crypto-button-container">
+                        <Button
+                          className="back-button"
+                          background="light"
+                          onClick={() => history.push(`/listings/${booking.listingId}`)}
+                        >
+                          Back
+                        </Button>
+                        <SelectPaymentButton
+                          booking={booking}
+                          currency={currency}
+                          disabled={hasInsufficientFunds}
+                          onSuccess={() => history.push(`/bookings/${booking.id}/payment`)}
+                        />
+                      </div>
                     </div>
                   );
-                }
-                return (
-                  <div>
-                    {accounts && (
-                      <div className="booking-options-disclaimer">
-                        <p>
-                          <span>Note:&nbsp;</span>
-                          Wallet Address is automatically updated to the current wallet selected in your Metamask
-                          browser application. Make sure the correct wallet is selected and matches the wallet address
-                          below.
-                        </p>
-                      </div>
-                    )}
-                    <div className="crypto-button-container">
-                      <Button
-                        className="back-button"
-                        background="light"
-                        onClick={() => history.push(`/listings/${booking.listingId}`)}
-                      >
-                        Back
-                      </Button>
-                      <SelectPaymentButton
-                        booking={booking}
-                        currency={currency}
-                        disabled={hasInsufficientFunds}
-                        onSuccess={() => history.push(`/bookings/${booking.id}/payment`)}
-                      />
-                    </div>
-                  </div>
-                );
-              }}
-            </AppConsumer>
-          </BookingOptionsCryptoContainer>
+                }}
+              </AppConsumer>
+            </BookingOptionsCryptoContainer>
+          );
+        };
+        return (
         );
       }}
     </Web3Consumer>
@@ -142,7 +146,7 @@ const BookingOptionsCrypto = ({ booking, currency, fromBee, history }: Props) =>
 
 export default withRouter(BookingOptionsCrypto);
 
-function getAvailableAmount(accounts: Web3Data['accounts'], currency: Currency): number {
+async function getAvailableAmount(accounts: Web3Data['accounts'], currency: Currency): Promise<number> {
   if (!accounts) {
     return 0;
   }
@@ -152,6 +156,9 @@ function getAvailableAmount(accounts: Web3Data['accounts'], currency: Currency):
       return account.availableBee;
     case Currency.ETH:
       return account.availableEth;
+    case Currency.DAI:
+      const web3 = loadWeb3();
+      return balanceOf(web3.eth, currency, accounts[0].walletAddress);
     default:
       return 0;
   }
