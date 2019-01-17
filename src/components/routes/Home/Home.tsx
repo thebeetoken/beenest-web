fe1942278965340520d34a9e5570534f3d36aab0
Tommy Chheng <tommy.chheng@gmail.com>
2019-01-16 16:08:03 -0800

    text changes

--- src/components/routes/Home/Home.tsx
+++ src/components/routes/Home/Home.tsx
@@ -23,7 +23,7 @@ class Home extends React.Component {
           <div className="text-container">
             <h1>Book Rentals and Homes for your Business</h1>
             <p>
-              Beenest is the easiest way to spend and earn when you travel.
+               Beenest is the easiest way for businesses to book a home or vacation rental.
             </p>
             <AppConsumer>
               {({ screenType }: AppConsumerProps) => {
