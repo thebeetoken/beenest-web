e29cdf1c71c4e7a2ed667425e52951bcabd1d444
Jeremy Kwa <jeremykwa@gmail.com>
2019-01-25 14:17:48 -0800

    remove duplicate code, tabnav size adjusts to content within

--- src/components/routes/Trips/Trips.tsx
+++ src/components/routes/Trips/Trips.tsx
@@ -79,7 +79,7 @@ class Trips extends React.Component<Props, State> {
                       to: '/trips/past',
                     },
                     {
-                      title: 'Cancelled /\nRejected',
+                      title: 'Cancelled / Rejected',  // \xa0 is a non-breaking space
                       to: '/trips/cancelled',
                     }
                   ]} />
--- src/components/shared/TabNav/TabNav.container.ts
+++ src/components/shared/TabNav/TabNav.container.ts
@@ -6,56 +6,64 @@ type Props = Partial<{
   tabWidth: number;
 }>;
 
-const TabNavContainer = styled.div`
-  display: flex;
+const TabNavContainerMobile = styled.div`
+  display: inline-block;
   height: ${({ height }: Props) => height ? `${height}px` : '100%'};
-  width: 100%;
   flex-grow: 1;
   position: relative;
 
-  .bee-tab-nav-item--container {
-    flex-grow: 1;
-    width: 100%;
-    height: 100%;
-    position: relative;
-    .alert-badge {
-      ${typography('title', 3)}
-      color: ${color('error')};
-      position: absolute;
-      right: 16px;
-      top: -16px;
-      z-index: 2;
+
+  .alert-badge {
+    ${typography('title', 3)}
+    color: ${color('error')};
+    position: absolute;
+    right: 16px;
+    top: -16px;
+    z-index: 2;
+  }
+  a {
+    ${typography('title', 7)}
+    align-items: center;
+    background-color: ${color('light')};
+    color: ${color('upper')};
+    display: flex;
+    height: inherit;
+    justify-content: center;
+    transition: all 0.2s ease-in-out;
+    width: inherit;
+    &:hover:not(.active) {
+      opacity: 0.5;
     }
-    .bee-tab-nav--item {
-      align-items: center;
-      display: flex;
-      height: inherit;
-      justify-content: center;
-      width: ${({ tabWidth }: Props) => tabWidth ? `${tabWidth}px` : '100%'};
-      a {
+    &.active {
+      background-color: ${color('white')};
+      box-shadow: 0 2px 10px ${color('black', 0.1)};
+      border-radius: 2px;
+      color: ${color('secondary')};
+      position: relative;
+      z-index: 1;
+    }
+    span {
+      ${typography('read', 3)}
+      text-align: center;
+      word-break: break-word;
+    }
+    .bee-svg {
+      height: 24px;
+      width: 24px;
+    }
+  }
+`;
+
+const TabNavContainerTablet = styled(TabNavContainerMobile)`
+  @media (min-width: 768px) {
+    a {
+      padding: 0 16px;
+      span {
         ${typography('title', 7)}
-        align-items: center;
-        background-color: ${color('light')};
-        color: ${color('upper')};
-        display: flex;
-        height: inherit;
-        justify-content: center;
-        transition: all 0.2s ease-in-out;
-        width: inherit;
-        &:hover:not(.active) {
-          opacity: 0.5;
-        }
-        &.active {
-          background-color: ${color('white')};
-          box-shadow: 0 2px 10px ${color('black', 0.1)};
-          border-radius: 2px;
-          color: ${color('secondary')};
-          position: relative;
-          z-index: 1;
-        }
+        word-break: normal;
       }
     }
   }
 `;
 
-export default TabNavContainer;
+export default TabNavContainerTablet;
