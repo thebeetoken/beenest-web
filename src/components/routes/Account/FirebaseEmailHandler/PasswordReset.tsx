b9a9d69e484caa5aa10c895be120cce1e6f70900
Tommy Chheng <tommy.chheng@gmail.com>
2019-01-30 15:33:08 -0800

    prettier

--- src/components/routes/Account/FirebaseEmailHandler/PasswordReset.tsx
+++ src/components/routes/Account/FirebaseEmailHandler/PasswordReset.tsx
@@ -38,72 +38,112 @@ export default class PasswordReset extends React.Component<Props> {
       return;
     }
 
-    // @see https://firebase.google.com/docs/auth/custom-email-handler
+    // @see
+    // https://firebase.google.com/docs/auth/custom-email-handler
     this.handleResetPassword(oobCode);
   }
 
   handleFormChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = event.target;
-    return this.setState({[name]: value });
-  }
-
-  // @see https://firebase.google.com/docs/auth/custom-email-handler
-  handleResetPassword(actionCode:string) {
-    // Verify the password reset code is valid.
-    auth.verifyPasswordResetCode(actionCode).then(() => {
-      this.setState({showPasswordResetForm: true});
-    }).catch(() => {
-      this.setState({hasError: true, errorMessage: 'Invalid or expired action code. Ask user to try to reset the password again.'});
+    return;
+    this.setState({
+      [name]: value,
     });
+  };
+
+  // @see
+  // https://firebase.google.com/docs/auth/custom-email-handler
+  handleResetPassword(actionCode: string) {
+    // Verify the password reset code
+    // is valid.
+    auth
+      .verifyPasswordResetCode(actionCode)
+      .then(() => {
+        this.setState({ showPasswordResetForm: true });
+      })
+      .catch(() => {
+        this.setState({
+          hasError: true,
+          errorMessage: 'Invalid or expired action code.Ask user to try to reset the password again.',
+        });
+      });
   }
 
   handlePasswordResetSubmit(event: React.FormEvent) {
     event.preventDefault();
 
     // Save the new password.
-    auth.confirmPasswordReset(this.props.oobCode, this.state.password).then(() => {
-      // Password reset has been confirmed and new password updated.
-      // Redirect user back to /login to confirm their password worked
-      this.setState({showPasswordResetSuccess: true});
-    }).catch(() => {
-      this.setState({hasError: true, errorMessage: 'Password is too weak or the password reset link has expired.'});
-    });
+    auth
+      .confirmPasswordReset(this.props.oobCode, this.state.password)
+      .then(() => {
+        // Password reset has been
+        // confirmed and new
+        // password updated.
+        // Redirect user back
+        // to /login to
+        // confirm their
+        // password worked
+        this.setState({
+          showPasswordResetSuccess: true,
+        });
+      })
+      .catch(() => {
+        this.setState({
+          hasError: true,
+          errorMessage: 'Password is too weak or the password reset link has expired.',
+        });
+      });
   }
 
   renderError() {
-    return <>
-      <h2>Sorry, there was an error.</h2>
-      <p>{this.state.errorMessage}</p>
-      <p>
-        <BeeLink href="mailto:support@beenest.com">
-          <Button>Contact us for further help.</Button>
-        </BeeLink>
-      </p>
-    </>;
+    return (
+      <>
+        <h2> Sorry, there was an error.</h2>
+        <p>{this.state.errorMessage}</p>
+        <p>
+          {' '}
+          <BeeLink href="mailto:support@beenest.com">
+            <Button>Contact us for further help.</Button>
+          </BeeLink>{' '}
+        </p>{' '}
+      </>
+    );
   }
 
   renderPasswordResetForm() {
-      return <>
-          <h2>Reset Password</h2>
-              <form onSubmit={this.handlePasswordResetSubmit}>
-                 <div className="input-container">
-                   <InputWrapper>
-                   <label>New Password:</label>
-                   <input type="password" value={this.state.password} onChange={this.handleFormChange} id="password" name="password" />
-                   </InputWrapper>
-                 </div>
-                 <Button type="submit">Save</Button>
-             </form>
-          </>;
+    return;
+    <>
+      {' '}
+      <h2> Reset Password</h2>
+      <form onSubmit={this.handlePasswordResetSubmit}>
+        <div className="input-container">
+          <InputWrapper>
+            {' '}
+            <label>New Password:</label>{' '}
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.handleFormChange}
+              id="password"
+              name="password"
+            />{' '}
+          </InputWrapper>{' '}
+        </div>
+        <Button type="submit">Save</Button>
+      </form>{' '}
+    </>;
   }
 
   renderPasswordResetSuccess() {
-                return <section>
-                        <p>You changed your password.</p>
-                        <BeeLink href="/login">
-                          <Button>Login</Button>
-                        </BeeLink>
-                      </section>;
+    return (
+      <section>
+        {' '}
+        <p>You changed your password.</p>{' '}
+        <BeeLink href="/login">
+          <Button>Login</Button>
+        </BeeLink>{' '}
+      </section>
+    );
   }
 
   render() {
@@ -112,11 +152,12 @@ export default class PasswordReset extends React.Component<Props> {
     }
 
     return (
-          <>
-            {this.state.hasError && this.renderError()}
-            {this.state.showPasswordResetForm && this.renderPasswordResetForm()}
-            {this.state.showPasswordResetSuccess && this.renderPasswordResetSuccess()}
-          </>
+      <>
+        {' '}
+        {this.state.hasError && this.renderError()}
+        {this.state.showPasswordResetForm && this.renderPasswordResetForm()}
+        {this.state.showPasswordResetSuccess && this.renderPasswordResetSuccess()}
+      </>
     );
   }
-};
+}
