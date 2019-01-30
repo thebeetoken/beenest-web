1371ac50ed194b5136a18e2ce13cc6c7d02ce4b3
Vic Woeltjen <vic@thebeetoken.com>
2019-01-30 14:22:02 -0800

    Add breaks in explanatory text

--- src/components/routes/Host/HostPayments/HostPayments.tsx
+++ src/components/routes/Host/HostPayments/HostPayments.tsx
@@ -142,7 +142,7 @@ class EnhancedComponent extends React.Component<Props, HostPaymentsContentState>
               </Button>
             </BeeLink>
             {!stripeAccountDashboardLink && <p>
-              This is only available to hosts in the United States and Canada. This account will be limited to receiving payouts from Beenest.
+              This is only available to hosts in the United States and Canada.<br/>This account will be limited to receiving payouts from Beenest.
             </p>}
             {!stripeAccountDashboardLink && <BeeLink to="/account/stripe/connect" target="_blank">
               <Button background="secondary" color="white" size="small">
@@ -150,7 +150,7 @@ class EnhancedComponent extends React.Component<Props, HostPaymentsContentState>
               </Button>
             </BeeLink>}
             {!stripeAccountDashboardLink && <p>
-              Available in the United States and other countries. Select this if you have an existing Stripe account.
+              Available in the United States and other countries.<br/>Select this if you have an existing Stripe account.
             </p>}
           </div>
           <Formik
