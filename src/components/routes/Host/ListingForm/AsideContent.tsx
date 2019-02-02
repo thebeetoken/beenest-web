df9a8d8224c3c24400c4604d4d89929ea6474d4e
Jeremy Kwa <jeremykwa@gmail.com>
2019-02-01 17:40:49 -0800

    add titles and more help text to first 2 tabs

--- src/components/routes/Host/ListingForm/AsideContent.tsx
+++ src/components/routes/Host/ListingForm/AsideContent.tsx
@@ -6,6 +6,8 @@ interface AsideContentInterface {
 
 const HomeType = () => (
   <div>
+    <h3>Home Type</h3>
+    <br />
     <strong>Entire place</strong>
     <p>The whole space is dedicated to guests. This may be a house, apartment, condo, loft, etc.</p>
     <br />
@@ -17,40 +19,76 @@ const HomeType = () => (
   </div>
 );
 
+const Title = () => (
+  <div>
+    <h3>Listing Name</h3>
+    <br />
+    <p>&quot;Beautiful Home with Wifi and Views&quot;</p>
+  </div>
+);
+
+const Description = () => (
+  <div>
+    <h3>Listing Description</h3>
+    <br />
+    <p>Include a summary of your place to help guests imagine what it will be like to stay there. Note what makes your place special, mention what the neighborhood is like, and add anything else guests should know before arriving.</p>
+  </div>
+)
+
 const FullAddress = () => (
   <div>
-    <p>Don't forget to add a zip code!</p>
+    <h3>Full Address</h3>
+    <br />
+    <p>When adding the Full Address -- don’t forget to add a zip code!</p>
   </div>
 );
 
 const ListingPicUrl = () => (
   <div>
+    <h3>Cover Photo</h3>
+    <br />
     <p>High-quality photos are a must as our guests are typically business travelers. Ideal image dimensions are 1440x960. The maximum file size is 5 MB and the file types we accept are .jpg &amp; .png.</p>
   </div>
 );
 
 const Photos = () => (
   <div>
+    <h3>Listing Photos</h3>
+    <br />
     <p>Beenest recommends listings have at least 5 high definition photos available. Pro tip: provide multiple photos with different angles of bedrooms, bathrooms, and public spaces so guests know exactly what to expect.</p>
   </div>
 );
 
 const SleepingArrangement = () => (
   <div>
+    <h3>Sleeping Arrangement</h3>
+    <br />
     <p>The number and type of beds you have determine how many guests can stay comfortably.</p>
     <br />
-    <p>Sleeping arrangements help guests decide whether your space is a right fit and know what to expect once they’re there.</p>
+    <p>Sleeping Arrangement must include the quantity and size of beds.</p>
+  </div>
+);
+
+const NumberOfBedrooms = () => (
+  <div>
+    <h3>Number of Bedrooms</h3>
+    <br />
+    <p>Number of Bedrooms help guests decide whether your space is a right fit and know what to expect once they’re there.</p>
   </div>
 );
 
 const NumberOfBathrooms = () => (
   <div>
+    <h3>Number of Bathrooms</h3>
+    <br />
     <p>If you have a toilet separate from the shower, count it as a 0.5 bathroom. Count only the bathrooms guests can use.</p>
   </div>
 );
 
 const Amenities = () => (
   <div>
+    <h3>Amenities</h3>
+    <br />
     <p>Please make sure to include Wifi capabilities for business travelers. Providing the essentials helps guests feel at home in your place. Other common amenities include: TV, Heat, Air conditioning, Iron, Hair dryer, First aid kit, Parking on premises, etc.</p>
     <br />
     <p>There’s no maximum to the number of amenities you can include so add as many as you’d like! </p>
@@ -100,21 +138,21 @@ const HouseRules = () => (
 
 const AsideContent: AsideContentInterface = {
   homeType: <HomeType />,
-  title: <></>,
-  description: <></>,
+  title: <Title />,
+  description: <Description />,
   addressLine1: <FullAddress/>,
   addressLine2: <FullAddress/>,
   city: <FullAddress/>,
   state: <FullAddress/>,
   postalCode: <FullAddress/>,
-  country: <></>,
+  country: <FullAddress />,
   listingPicUrl: <ListingPicUrl />,
   photos: <Photos />,
   
   sleepingArrangement: <SleepingArrangement />,
-  numberOfBedrooms: <></>,
+  numberOfBedrooms: <NumberOfBedrooms />,
   numberOfBathrooms: <NumberOfBathrooms />,
-  sharedBathroom: <></>,
+  sharedBathroom: <NumberOfBathrooms />,
   amenities: <Amenities />,
 
   maxGuests: <></>,
