ec8331fc41f8d949d8b59c74a75cd17610eded00
Kevin Van <kevin@beetoken.com>
2019-02-11 13:51:12 -0800

    feat(theme,restructure): add scss theme and restructure work directory

--- src/work.tsx
+++ src/work.tsx
@@ -9,14 +9,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import 'styled/theme.css';
+import 'styled/theme/theme.scss';
 
 // Inject Global Styles
 import GlobalStyles from 'styled/globalStyles';
 import ApolloWrapper from 'HOCs/ApolloWrapper';
 import ErrorBoundaryWrapper from 'HOCs/ErrorBoundaryWrapper';
 import { FirebaseProvider } from 'HOCs/FirebaseProvider';
-import Work from './components/Work';
+import Work from './pages';
 
 ReactDOM.render(
   <>
