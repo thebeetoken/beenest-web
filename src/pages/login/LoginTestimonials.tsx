11d9d3227881732f4d6c59931e92c3cd0b63bc83
Kevin Van <kevin@beetoken.com>
2019-02-13 10:56:07 -0800

    fix(typo): fix typo

--- src/pages/login/LoginTestimonials.tsx
+++ src/pages/login/LoginTestimonials.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { Col } from 'reactstrap';
 
-const LoginTentimonials = () => (
+const LoginTestimonials = () => (
   <Col lg="5" xl="4" className="d-none d-lg-flex align-items-center gradient-half-primary-v1 height-lg-100vh px-0">
 
   </Col>
 );
 
-export default LoginTentimonials;
+export default LoginTestimonials;
