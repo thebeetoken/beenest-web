aa5e8f4466f68cac41ef58d412d938d6918f8a99
Jeremy Kwa <jeremykwa@gmail.com>
2019-02-13 15:17:31 -0800

    networking for general info

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -8,6 +8,8 @@ import {
   NavItem,
   NavLink,
   Container,
+  Col,
+  Row,
 } from 'reactstrap';
 import { Route, Redirect, Switch } from 'react-router';
 import NotFound from 'components/routes/NotFound';
@@ -15,6 +17,10 @@ import AccountGeneral from './AccountGeneral';
 import AccountPayment from './AccountPayment';
 import AccountSecurity from './AccountSecurity';
 import AccountVerification from './AccountVerification';
+import { GET_ACCOUNT_PAGE } from 'networking/users';
+import { Query } from 'react-apollo';
+import AudioLoading from 'shared/loading/AudioLoading';
+import { FirebaseConsumer, FirebaseUserProps } from 'HOCs/FirebaseProvider';
 
 class Account extends React.Component<any> {
   state = {
@@ -44,39 +50,65 @@ class Account extends React.Component<any> {
           </Collapse>
         </Navbar>
 
-        <Container>
-          <div>
-            <h1>Profile</h1>
-            <hr />
-            <Nav>
-              <NavItem>
-                <NavLink href="/work/account/general">General Info</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/work/account/payment">Payment</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/work/account/security">Security</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/work/account/verification">Verification</NavLink>
-              </NavItem>
-            </Nav>
-
-            <Switch>
-              <Route exact path="/work/account/general" render={() => <AccountGeneral />} />
-              <Route exact path="/work/account/payment" render={() => <AccountPayment />} />
-              <Route exact path="/work/account/security" component={AccountSecurity} />
-              <Route exact path="/work/account/verification" component={AccountVerification} />
-              <Redirect exact from="/work/account" to="/work/account/general" />
-              <Route component={NotFound} />
-            </Switch>
-          </div>
-        </Container>
-
+        <Query query={GET_ACCOUNT_PAGE}>
+          {({ loading, error, data }) => {
+            if (loading) {
+              return <AudioLoading height={48} width={96} />;
+            }
+            if (error || !data) {
+              return <h1>{error ? error.message : 'Error / No Data'}</h1>;
+            }
+            const { user } = data;
+            return (
+              <Container>
+                <h1>Profile</h1>
+                <hr />
+                <Nav>
+                  <NavItem>
+                    <NavLink href="/work/account/general">General Info</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/work/account/payment">Payment</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/work/account/security">Security</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/work/account/verification">Verification</NavLink>
+                  </NavItem>
+                </Nav>
+                
+                <Container>
+                  <Row>
+                    <Col md={6}>
+                      <hr />
+                      <Switch>
+                        <Route exact path="/work/account/general" render={(props: RouterProps) => <AccountGeneral {...props} user={user} />} />
+                        <Route exact path="/work/account/payment" render={() => <AccountPayment />} />
+                        <Route exact path="/work/account/security" component={AccountSecurity} />
+                        <Route exact path="/work/account/verification" component={AccountVerification} />
+                        <Redirect exact from="/work/account" to="/work/account/general" />
+                        <Route component={NotFound} />
+                      </Switch>
+                    </Col>
+                  </Row>
+                </Container>
+              </Container>
+            );
+          }}
+        </Query>
       </div>
     );
   }
 }
 
-export default Account;
+export default () => (
+  <FirebaseConsumer>
+    {({ user, loading }: FirebaseUserProps) => {
+      if (loading) {
+        return null;
+      }
+      return user ? <Account /> : <Redirect to="/login" />;
+    }}
+  </FirebaseConsumer>
+);
