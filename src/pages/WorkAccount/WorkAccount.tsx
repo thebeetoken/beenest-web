3ad8eacf04ecf7506d31a8ebb5f4cddc3d604dea
Jeremy Kwa <jeremykwa@gmail.com>
2019-02-13 12:26:08 -0800

    basic nav infrastructure

---
+++ src/pages/WorkAccount/WorkAccount.tsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+  Button,
+} from 'reactstrap';
+import { Route, Redirect, Switch } from 'react-router';
+import NotFound from 'components/routes/NotFound';
+
+class WorkAccount extends React.Component<any> {
+  state = {
+    isOpen: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="inverse" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+
+        <div>
+          <p>Profile</p>
+          <hr />
+          <Nav>
+            <NavItem>
+              <NavLink href="/work/account/general">General Info</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/work/account/payment">Payment</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/work/account/security">Security</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/work/account/verification">Verification</NavLink>
+            </NavItem>
+          </Nav>
+
+          <Switch>
+            <Route exact path="/work/account/general" render={() => <h1>This is the General Info Page</h1>} />
+            <Route exact path="/work/account/payment" render={() => <h1>This is the Payment Page</h1>} />
+            <Route exact path="/work/account/security" component={() => <h1>This is the Security Page</h1>} />
+            <Route exact path="/work/account/verification" component={() => <h1>This is the Verification Page</h1>} />
+            <Redirect exact from="/work/account" to="/work/account/general" />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default WorkAccount;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './home';
+import WorkAccount from './WorkAccount';
 
 const Work = () => (
   <div>
     <Switch>
+      <Route path="/work/account" component={WorkAccount} />
       <Route path="/work" component={Home} />
     </Switch>
   </div>
