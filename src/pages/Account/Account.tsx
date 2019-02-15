c661b058e6687969561d9a3f2a3c1b0cc0dce0e6
Jeremy Kwa <jeremykwa@gmail.com>
2019-02-15 14:19:26 -0800

    make nav into array of obj

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -1,12 +1,5 @@
 import * as React from 'react';
-import {
-  Nav,
-  NavItem,
-  NavLink,
-  Container,
-  Col,
-  Row,
-} from 'reactstrap';
+import { Nav, NavItem, NavLink, Container, Col, Row } from 'reactstrap';
 import { Query } from 'react-apollo';
 import { Route, Redirect, Switch } from 'react-router';
 import { NavLink as RRNavLink } from 'react-router-dom';
@@ -31,50 +24,58 @@ const Account = () => {
         if (error || !data) {
           return <h1>{error ? error.message : 'Error / No Data'}</h1>;
         }
-        
+
         const { creditBalance, user } = data;
         return (
           <Container>
             <h1>Profile</h1>
             <hr />
             <Nav tabs>
-              <NavItem>
-                <NavLink
-                  tag={RRNavLink}
-                  to="/work/account/general">
-                  General Info
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  tag={RRNavLink}
-                  to="/work/account/payment">
-                  Payment
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  tag={RRNavLink}
-                  to="/work/account/security">
-                  Security
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  tag={RRNavLink}
-                  to="/work/account/verification">
-                  Verification
-                </NavLink>
-              </NavItem>
+              {[
+                {
+                  tag: RRNavLink,
+                  to: '/work/account/general',
+                  title: 'General Info',
+                },
+                {
+                  tag: RRNavLink,
+                  to: '/work/account/payment',
+                  title: 'Payment',
+                },
+                {
+                  tag: RRNavLink,
+                  to: '/work/account/security',
+                  title: 'Security',
+                },
+                {
+                  tag: RRNavLink,
+                  to: '/work/account/verification',
+                  title: 'Verification',
+                },
+              ].map(({ title, tag, to }) => (
+                <NavItem>
+                  <NavLink tag={tag} to={to}>
+                    {title}
+                  </NavLink>
+                </NavItem>
+              ))}
             </Nav>
-            
+
             <Container>
               <Row>
                 <Col md={6}>
                   <hr />
                   <Switch>
-                    <Route exact path="/work/account/general" render={(props: RouterProps) => <AccountGeneral {...props} user={user} />} />
-                    <Route exact path="/work/account/payment" render={(props: RouterProps) => <AccountPayment {...props} creditBalance={creditBalance} />} />
+                    <Route
+                      exact
+                      path="/work/account/general"
+                      render={(props: RouterProps) => <AccountGeneral {...props} user={user} />}
+                    />
+                    <Route
+                      exact
+                      path="/work/account/payment"
+                      render={(props: RouterProps) => <AccountPayment {...props} creditBalance={creditBalance} />}
+                    />
                     <Route exact path="/work/account/security" component={AccountSecurity} />
                     <Route exact path="/work/account/verification" component={AccountVerification} />
                     <Redirect exact from="/work/account" to="/work/account/general" />
@@ -88,7 +89,7 @@ const Account = () => {
       }}
     </Query>
   );
-}
+};
 
 export default () => (
   <FirebaseConsumer>
