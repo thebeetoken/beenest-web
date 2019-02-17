b448e462b40b14a25545395551959ae567ff0143
Jeremy Kwa <jeremykwa@gmail.com>
2019-02-16 22:03:26 -0800

    vertically center stripe inputs inside bootstrap

--- src/pages/Account/NewCardForm.tsx
+++ src/pages/Account/NewCardForm.tsx
@@ -6,12 +6,19 @@ import * as Yup from 'yup';
 import { compose, graphql } from 'react-apollo';
 import { injectStripe, CardNumberElement, CardExpiryElement, CardCVCElement, PostalCodeElement } from 'react-stripe-elements';
 import StripeWrapper from 'HOCs/StripeWrapper';
+import styled from 'styled-components';
 
 interface Props {
   stripe: Window['Stripe'];
   createPaymentSource: (stripeToken: string) => void;
   onClose: () => void;
 }
+
+const StripeInputContainer = styled.div`
+  .__PrivateStripeElement {
+    width: 100%;
+  }
+`
 
 enum NewCardField {
   CARD_NUMBER = 'cardNumber',
@@ -23,12 +30,12 @@ enum NewCardField {
 }
   
 const NewCardSchema = Yup.object().shape({
-  [NewCardField.CARD_NUMBER]: Yup.string().required(),
-  [NewCardField.CVC]: Yup.string().required(),
-  [NewCardField.ID]: Yup.string().required(),
+  [NewCardField.CARD_NUMBER]: Yup.string(),
+  [NewCardField.CVC]: Yup.string(),
+  [NewCardField.ID]: Yup.string(),
   [NewCardField.NAME_ON_CARD]: Yup.string().required(),
-  [NewCardField.CARD_EXPIRY]: Yup.string().required(),
-  [NewCardField.ZIP]: Yup.string().required(),
+  [NewCardField.CARD_EXPIRY]: Yup.string(),
+  [NewCardField.ZIP]: Yup.string(),
 });
 
 const NewCardForm = ({ createPaymentSource, stripe }: Props) => (
@@ -44,13 +51,13 @@ const NewCardForm = ({ createPaymentSource, stripe }: Props) => (
     }}
     validationSchema={NewCardSchema}
     onSubmit={(values, actions) => {
-      return console.log('values:', values);
       return stripe
         .createToken(values)
         .then(({ token }: any) => {
           return createPaymentSource(token.id);
         })
-        .then(() => {
+        .then((payload: any) => {
+          console.log('[token]', payload);
           alert('Credit card successfully added.');
         })
         .catch((error: Error) => {
@@ -59,7 +66,7 @@ const NewCardForm = ({ createPaymentSource, stripe }: Props) => (
         })
         .finally(() => actions.setSubmitting(false));
     }}>
-    {({ errors, isSubmitting, submitForm, touched }: FormikProps<any>) => (
+    {({ errors, isSubmitting, setFieldTouched, submitForm, touched }: FormikProps<any>) => (
       <Form method="POST">
         <Row>
           <Col>
@@ -80,30 +87,38 @@ const NewCardForm = ({ createPaymentSource, stripe }: Props) => (
           <Col md={6}>
             <FormGroup>
               <Label for={NewCardField.CARD_NUMBER} className="form-label">Card Number</Label>
-              <Input
-                id={NewCardField.CARD_NUMBER}
-                name={NewCardField.CARD_NUMBER}
-                tag={CardNumberElement} />
+              <StripeInputContainer>
+                <CardNumberElement
+                  className="form-control d-flex align-items-center"
+                  id={NewCardField.CARD_NUMBER}
+                  onBlur={() => setFieldTouched(NewCardField.CARD_NUMBER)}/>
+              </StripeInputContainer>
               <FormFeedback>{errors.cardNumber}</FormFeedback>
             </FormGroup>
           </Col>
           <Col md={3}>
             <FormGroup>
               <Label for={NewCardField.CARD_EXPIRY} className="form-label">Card Expiry</Label>
-              <Input
-                id={NewCardField.CARD_EXPIRY}
-                name={NewCardField.CARD_EXPIRY}
-                tag={CardExpiryElement} />
+              <StripeInputContainer>
+                <Input
+                  className="form-control d-flex align-items-center"
+                  id={NewCardField.CARD_EXPIRY}
+                  name={NewCardField.CARD_EXPIRY}
+                  tag={CardExpiryElement} />
+              </StripeInputContainer>
               <FormFeedback>{errors.cardExpiry}</FormFeedback>
             </FormGroup>
           </Col>
           <Col md={3}>
             <FormGroup>
             <Label for={NewCardField.CVC} className="form-label">CVC</Label>
-              <Input
-                id={NewCardField.CVC}
-                name={NewCardField.CVC}
-                tag={CardCVCElement} />
+              <StripeInputContainer>
+                <Input
+                  className="form-control d-flex align-items-center"
+                  id={NewCardField.CVC}
+                  name={NewCardField.CVC}
+                  tag={CardCVCElement} />
+              </StripeInputContainer>
               <FormFeedback>{errors.CVC}</FormFeedback>
             </FormGroup>
           </Col>
@@ -113,10 +128,13 @@ const NewCardForm = ({ createPaymentSource, stripe }: Props) => (
           <Col>
             <FormGroup>
               <Label for={NewCardField.ZIP} className="form-label">ZIP</Label>
-                <Input
-                  id={NewCardField.ZIP}
-                  name={NewCardField.ZIP}
-                  tag={PostalCodeElement} />
+                <StripeInputContainer>
+                  <Input
+                    className="form-control d-flex align-items-center"
+                    id={NewCardField.ZIP}
+                    name={NewCardField.ZIP}
+                    tag={PostalCodeElement} />
+                </StripeInputContainer>
                 <FormFeedback>{errors.zip}</FormFeedback>
             </FormGroup>
           </Col>
@@ -131,7 +149,6 @@ const NewCardForm = ({ createPaymentSource, stripe }: Props) => (
               className="btn-success transition-3d-hover"
               color="success"
               onClick={() => {
-                console.log('errors:', errors);
                 submitForm();
               }}
               type="button">
