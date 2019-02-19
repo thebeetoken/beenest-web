7091690981ca4a00316b302c2573adabd7fbd6e4
Jeremy Kwa <jeremykwa@gmail.com>
2019-02-18 19:23:48 -0800

    add alerts

--- src/pages/Account/AccountSecurity.tsx
+++ src/pages/Account/AccountSecurity.tsx
@@ -1,21 +1,71 @@
 import * as React from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem, Alert } from 'reactstrap';
 import { resetPassword } from 'utils/firebase';
+import { getDisplaySuccessMessage, SuccessMessage, getDisplayErrorMessage, ErrorMessage } from 'utils/validators';
+import { FirebaseConsumer, FirebaseUserProps } from 'HOCs/FirebaseProvider';
+import { User } from 'firebase';
 
-class AccountSecurity extends React.Component<any> {
-  render() {
-    return (
-      <ListGroup className="mb-2 d-flex flex-column">
-        <ListGroupItem className="w-100 d-flex flex-column">
-          <h6 className="mb-0">Reset Password</h6>
-          <div className="d-flex justify-content-between">
-            <h6 className="mb-0 small text-muted">Click here to reset your password</h6>
-            <i className="fa fa-lock"></i>
-          </div>
-        </ListGroupItem>
-      </ListGroup>
-    );
+interface AlertProperties {
+  color: string;
+  msg: string;
+  show: boolean;
+}
+
+function AccountSecurity() {
+  const [alert, setAlert] = React.useState<AlertProperties>({ color: '', msg: '', show: false });
+  const [isSubmitting, setSubmitting] = React.useState<boolean>(false);
+
+  function handleClick(user: User) {
+    if (!user.email) return;
+    setSubmitting(true);
+    resetPassword(user.email)
+      .then(() => {
+        setAlert({
+          color: 'success',
+          msg: getDisplaySuccessMessage(SuccessMessage.CHECK_EMAIL),
+          show: true,
+        });
+      })
+      .catch(() => {
+        setAlert({
+          color: 'danger',
+          msg: getDisplayErrorMessage(ErrorMessage.GENERIC),
+          show: true,
+        });
+      })
+      .finally(() => {
+        setSubmitting(false);
+      });
   }
+
+  return (
+    <>
+      <FirebaseConsumer>
+        {({ user }: FirebaseUserProps) => (
+          <ListGroup className="mb-2 d-flex flex-column">
+            {user &&
+              <ListGroupItem
+                className="w-100 d-flex flex-column"
+                disabled={isSubmitting}
+                onClick={() => handleClick(user)}>
+                <h6 className="mb-0">Reset Password</h6>
+                <div className="d-flex justify-content-between">
+                  <h6 className="mb-0 small text-muted">Click here to reset your password</h6>
+                  <i className="fa fa-lock"></i>
+                </div>
+              </ListGroupItem>
+            }
+          </ListGroup>
+        )}
+      </FirebaseConsumer>
+
+      <Alert
+        color={alert.color}
+        isOpen={alert.show}>
+        {alert.msg}
+      </Alert>
+    </>
+  );
 }
 
 export default AccountSecurity;
