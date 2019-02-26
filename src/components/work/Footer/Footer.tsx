1369d885741f4f1c7a7ecfc480fc803c130d0275
kevin <kevin@tommy-mbp-2.local>
2019-02-26 14:31:35 -0800

    fix(save): save file

--- src/components/work/Footer/Footer.tsx
+++ src/components/work/Footer/Footer.tsx
@@ -18,16 +18,16 @@ import { BeenestSVGPrimary } from 'shared/svgComponents/SvgComponents';
 
 const contentData = [
   {
-    header: 'Account',
-    links: [['Profile', '/work'], ['User Contacts', '/work'], ['Projects', '/work'], ['Settings', '/work']],
+    header: 'Top Cities',
+    links: [['San Francisco', '/work'], ['New York', '/work'], ['Los Angeles', '/work'], ['Denver', '/work'], ['Chicago', '/work']],
   },
   {
     header: 'Company',
-    links: [['About', '/work'], ['Services', '/work'], ['Careers', '/work'], ['Blog', '/work']],
+    links: [['About', '/work'], ['Blog', '/work']],
   },
   {
-    header: 'Resources',
-    links: [['Invoice', '/work'], ['Help', '/work'], ['Terms', '/work'], ['Privacy', '/work']],
+    header: 'Community',
+    links: [['Telegram', '/work'], ['Twitter', '/work'], ['Facebook', '/work']],
   },
 ];
 
