199cc2782d8d636011ef844cc7d7d11ae3134588
Vic Woeltjen <vic@thebeetoken.com>
2019-02-27 12:46:14 -0800

    Begin stubbing in SearchResults

---
+++ src/pages/search/SearchResults.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Query } from 'react-apollo';
+import { Col, Container, Row } from 'reactstrap';
+
+import { ListingShort } from 'networking/listings';
+
+interface Props {
+  listings: ListingShort[]
+}
+
+const SearchResults = ({ listings }: Props) => (
+
+);
+
+export default SearchForm;
