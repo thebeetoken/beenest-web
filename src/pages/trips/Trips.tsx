40d039a3d4b9c13a0fbc1e9a47cae538b2b9eea0
Jeremy Kwa <jeremykwa@gmail.com>
2019-02-27 15:46:01 -0800

    building card WIP

--- src/pages/trips/Trips.tsx
+++ src/pages/trips/Trips.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Nav, NavItem, NavLink, Container, Col, Row } from 'reactstrap';
+import { Nav, NavItem, NavLink, Container, Col, Row, CardGroup, Card, CardImg, CardBody, CardTitle, CardFooter, CardSubtitle, CardText } from 'reactstrap';
 import { Query, compose, graphql } from 'react-apollo';
 import { Route, Redirect, Switch } from 'react-router';
 import { NavLink as RRNavLink } from 'react-router-dom';
@@ -9,6 +9,8 @@ import NotFound from 'components/routes/NotFound';
 import { GET_GUEST_SORTED_BOOKINGS, Booking, Currency, GUEST_CANCEL_BOOKING } from 'networking/bookings';
 import { differenceInDays } from 'date-fns';
 import { cancel, loadWeb3 } from 'utils/web3';
+import { formatAddress, formatGeolocationAddress } from 'utils/formatter';
+import { getUserBookingDisplayStatus } from 'utils/bookingsDisplayStatus';
 
 const Trips = () => {
   return (
@@ -58,57 +60,89 @@ const Trips = () => {
                 </NavItem>
               ))}
             </Nav>
-
-            <Container>
-              <Row>
-                <Col md={6}>
-                  <Switch>
-                    <Route
-                      exact
-                      path="/work/trips/current"
-                      component={() => (
-                        <>
-                          <h1>This is the current page</h1>
-                          <p>{JSON.stringify(current)}</p>
-                        </>
-                      )}
-                    />
-                    <Route
-                      exact
-                      path="/work/trips/upcoming"
-                      component={() => (
-                        <>
-                          <h1>This is the upcoming page</h1>
-                          <p>{JSON.stringify(upcoming)}</p>
-                        </>
-                      )}
-                    />
-                    <Route
-                      exact
-                      path="/work/trips/past"
-                      component={() => (
-                        <>
-                          <h1>This is the past page</h1>
-                          <p>{JSON.stringify(past)}</p>
-                        </>
-                      )}
-                    />
-                    <Route
-                      exact
-                      path="/work/trips/cancelled"
-                      component={() => (
-                        <>
-                          <h1>This is the cancelled page</h1>
-                          <p>{JSON.stringify(cancelled)}</p>
-                        </>
-                      )}
-                    />
-                    <Redirect exact from="/work/trips" to={isCurrentEmpty ? "/work/trips/upcoming" : "/work/trips/current"} />
-                    <Route component={NotFound} />
-                  </Switch>
-                </Col>
-              </Row>
-            </Container>
+            <Switch>
+              <Route
+                exact
+                path="/work/trips/current"
+                component={() => (
+                  <>
+                    <h1>This is the current page</h1>
+                    <div className="card-deck d-block d-md-flex">
+                      {current.map((booking: Booking) => {
+                        const { checkInDate, checkOutDate, id, listing, status } = booking;
+                        const { addressLine1, addressLine2, city, country, lat, lng, postalCode, state } = listing;
+                        const displayStatus = getUserBookingDisplayStatus(status);
+                        return (
+                          <Card
+                            key={booking.id}
+                            className="mb-5">
+                            <CardImg top width="100%" src={booking.listing.listingPicUrl} alt="Listing Cover Photo"/>
+                            <CardBody>
+                              <CardTitle>{booking.listing.title}</CardTitle>
+                              <CardSubtitle>
+                                {addressLine1 && formatAddress(addressLine1, addressLine2, city, state, country, postalCode)}
+                                {!addressLine1 && formatGeolocationAddress({ lat, lng, city, country })}
+                              </CardSubtitle>
+                              <CardText>
+                                Status: {displayStatus}
+                              </CardText>
+                              <CardText>
+                                Booking: <span>{id}</span>
+                              </CardText>
+                            </CardBody>
+                          <CardFooter className="text-center">
+                            <Row className="align-items-center">
+                              <Col xs="4" className="u-ver-divider">
+                                <h5 className="small font-weight-normal text-secondary mb-0">Contact Host</h5>
+                              </Col>
+                              <Col xs="4" className="u-ver-divider">
+                                <h5 className="small font-weight-normal text-secondary mb-0">Receipt</h5>
+                              </Col>
+                              <Col xs="4">
+                                <h5 className="small font-weight-normal text-secondary mb-0">Cancel Trip</h5>
+                              </Col>
+                            </Row>
+                          </CardFooter>
+                          </Card>
+                        );
+                      })}
+                    </CardGroup>
+                  </>
+                )}
+              />
+              <Route
+                exact
+                path="/work/trips/upcoming"
+                component={() => (
+                  <>
+                    <h1>This is the upcoming page</h1>
+                    <p>{JSON.stringify(upcoming)}</p>
+                  </>
+                )}
+              />
+              <Route
+                exact
+                path="/work/trips/past"
+                component={() => (
+                  <>
+                    <h1>This is the past page</h1>
+                    <p>{JSON.stringify(past)}</p>
+                  </>
+                )}
+              />
+              <Route
+                exact
+                path="/work/trips/cancelled"
+                component={() => (
+                  <>
+                    <h1>This is the cancelled page</h1>
+                    <p>{JSON.stringify(cancelled)}</p>
+                  </>
+                )}
+              />
+              <Redirect exact from="/work/trips" to={isCurrentEmpty ? "/work/trips/upcoming" : "/work/trips/current"} />
+              <Route component={NotFound} />
+            </Switch>
           </Container>
         );
       }}
