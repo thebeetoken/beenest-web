21f796aa93d0f7bc8a03b4b7630c03fcc79aaf33
Vic Woeltjen <vic@thebeetoken.com>
2019-02-27 15:01:49 -0800

    Add margin among search results

--- src/pages/search/SearchResults.tsx
+++ src/pages/search/SearchResults.tsx
@@ -12,7 +12,7 @@ interface Props {
 const SearchResults = ({ listings }: Props) => (
   <Row>
     {listings.map((listing, index) => (
-      <Col xs="12" md="6" lg="4" key={index}>
+      <Col xs="12" md="6" lg="4" key={index} className="mb-5">
         <ListingCard {...listing} />
       </Col>
     ))}
