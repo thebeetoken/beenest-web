2319bfb250748c4dfd37657182849f142d1134db
Vic Woeltjen <vic@thebeetoken.com>
2019-02-27 13:48:03 -0800

    Tweak listing card layout

--- src/pages/search/ListingCard.tsx
+++ src/pages/search/ListingCard.tsx
@@ -11,14 +11,14 @@ const ListingCard = ({
   title
 }: ListingShort) => (
   <Card>
-    <div className="embed-responsive embed-responsive-16by9">
+    <div className="embed-responsive embed-responsive-4by3">
       <div className="embed-responsive-item">
         <CardImg className="w-100" src={listingPicUrl} alt={`Photo of ${title}`} />
       </div>
     </div>
     <CardBody>
       <CardSubtitle>{homeType} &middot; {sleepingArrangement} </CardSubtitle>
-      <CardTitle>{title}</CardTitle>
+      <CardTitle><h4>{title}</h4></CardTitle>
       <CardText>${pricePerNightUsd} per night</CardText>
     </CardBody>
   </Card>
