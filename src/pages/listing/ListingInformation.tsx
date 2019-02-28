7c53b10f364f5e02d86f798c1646379a7a8a8bd4
Vic Woeltjen <vic@thebeetoken.com>
2019-02-27 17:43:44 -0800

    Size imagery

--- src/pages/listing/ListingInformation.tsx
+++ src/pages/listing/ListingInformation.tsx
@@ -9,7 +9,7 @@ const ListingInformation = ({
   photos,
   title
 }: Listing) => <Fade>
-  <Row className="w-100 px-0 mx-0" style={{ height: '66vh' }}>
+  <Row className="w-100 px-0 mx-0" style={{ height: '75vh' }}>
     <ImageGrid images={[listingPicUrl, ...photos]} />
   </Row>
   <h1>{title}</h1>
