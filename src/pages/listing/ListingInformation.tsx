d2b81e745fb40ef1dfa80861cb4b8fcc7e79d817
Vic Woeltjen <vic@thebeetoken.com>
2019-02-27 17:42:34 -0800

    Show some imagery

--- src/pages/listing/ListingInformation.tsx
+++ src/pages/listing/ListingInformation.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react';
+import { Fade, Row } from 'reactstrap';
+
 import { Listing } from 'networking/listings';
+import ImageGrid from 'shared/ImageGrid';
 
 const ListingInformation = ({
+  listingPicUrl,
+  photos,
   title
-}: Listing) => <h1>{title}</h1>;
+}: Listing) => <Fade>
+  <Row className="w-100 px-0 mx-0" style={{ height: '66vh' }}>
+    <ImageGrid images={[listingPicUrl, ...photos]} />
+  </Row>
+  <h1>{title}</h1>
+</Fade>;
+
 
 export default ListingInformation;
