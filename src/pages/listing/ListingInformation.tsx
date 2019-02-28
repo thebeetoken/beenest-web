f051975d6b76815b8f6e8869013389b42f5cdb07
Vic Woeltjen <vic@thebeetoken.com>
2019-02-27 18:08:33 -0800

    Add Accommodations info

--- src/pages/listing/ListingInformation.tsx
+++ src/pages/listing/ListingInformation.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Fade, Row } from 'reactstrap';
+import sanitizeHtml from 'sanitize-html';
 
 import { Listing } from 'networking/listings';
 import ImageGrid from 'shared/ImageGrid';
@@ -9,11 +10,21 @@ import { formatAddress } from 'utils/formatter';
 const DEFAULT_PROFILE_URL = 'https://static.beenest.com/images/app/misc/profile.png';
 
 const ListingInformation = ({
+  checkInTime,
+  checkOutTime,
   city,
   country,
+  description,
+  homeType,
   host,
   listingPicUrl,
+  minimumNights,
+  maxGuests,
+  numberOfBedrooms,
+  numberOfBathrooms,
   photos,
+  sharedBathroom,
+  sleepingArrangement,
   state,
   title
 }: Listing) => <Fade>
@@ -24,6 +35,20 @@ const ListingInformation = ({
   <h2>{formatAddress(city, state, country)}</h2>
   <h3>Host: {host.displayName}</h3>
   <LazyImage src={host.profilePicUrl || DEFAULT_PROFILE_URL} width="5rem" height="5rem" />
+  <h1>Description</h1>
+  <p dangerouslySetInnerHTML={{ __html: sanitizeHtml(description) }} />
+  <h1>Accommodations</h1>
+  <dl>
+    <dt>Home Type</dt><dd>{homeType}</dd>
+    <dt>Sleeping arrangement</dt><dd>{sleepingArrangement}</dd>
+    <dt>Number of bedrooms</dt><dd>{numberOfBedrooms}</dd>
+    <dt>Number of bathrooms</dt><dd>{numberOfBathrooms}</dd>
+    <dt>Shared bathroom</dt><dd>{sharedBathroom}</dd>
+    <dt>Maximum number of guests</dt><dd>{maxGuests}</dd>
+    <dt>Minimum number of nights</dt><dd>{minimumNights}</dd>
+    <dt>Check-in</dt><dd>{checkInTime.from} to {checkInTime.to}</dd>
+    <dt>Check-out</dt><dd>{checkOutTime}</dd>
+  </dl>
 </Fade>;
 
 
