56dbc1a8a2fb860824df273f6fbe378ae4d4893c
Vic Woeltjen <vic@thebeetoken.com>
2019-02-27 17:53:26 -0800

    Bring in more Listing Info

--- src/pages/listing/ListingInformation.tsx
+++ src/pages/listing/ListingInformation.tsx
@@ -3,16 +3,23 @@ import { Fade, Row } from 'reactstrap';
 
 import { Listing } from 'networking/listings';
 import ImageGrid from 'shared/ImageGrid';
+import { formatAddress } from 'utils/formatter';
 
 const ListingInformation = ({
+  city,
+  country,
+  host,
   listingPicUrl,
   photos,
+  state,
   title
 }: Listing) => <Fade>
   <Row className="w-100 px-0 mx-0" style={{ height: '75vh' }}>
     <ImageGrid images={[listingPicUrl, ...photos]} />
   </Row>
   <h1>{title}</h1>
+  <h2>{formatAddress(city, state, country)}</h2>
+  <h3>Host: {host.displayName}</h3>
 </Fade>;
 
 
