59243e7b5bce06d8f3d5c243117a7389d30edb88
Vic Woeltjen <vic@thebeetoken.com>
2019-02-27 17:16:08 -0800

    Get listing ID

--- src/pages/listing/Listing.tsx
+++ src/pages/listing/Listing.tsx
@@ -4,11 +4,11 @@ import { Col, Fade, Row } from 'reactstrap';
 import Footer from 'components/work/Footer';
 import Header from 'components/work/Header';
 
-const Listing = () => (
+const Listing = ({ match }: RouterProps) => (
   <Fade>
     <Header />
     <Row className="w-100">
-      <Col>Hello, Listing.</Col>
+      <Col>Hello, {match.params.id}.</Col>
     </Row>
     <Footer />
   </Fade>
