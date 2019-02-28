d444c7375ba4b9e992058d82c414afa1b97a6a10
Vic Woeltjen <vic@thebeetoken.com>
2019-02-27 18:01:29 -0800

    Add profile pic (sans circle)

--- src/pages/listing/ListingInformation.tsx
+++ src/pages/listing/ListingInformation.tsx
@@ -3,7 +3,10 @@ import { Fade, Row } from 'reactstrap';
 
 import { Listing } from 'networking/listings';
 import ImageGrid from 'shared/ImageGrid';
+import LazyImage from 'shared/LazyImage';
 import { formatAddress } from 'utils/formatter';
+
+const DEFAULT_PROFILE_URL = 'https://static.beenest.com/images/app/misc/profile.png';
 
 const ListingInformation = ({
   city,
@@ -20,6 +23,7 @@ const ListingInformation = ({
   <h1>{title}</h1>
   <h2>{formatAddress(city, state, country)}</h2>
   <h3>Host: {host.displayName}</h3>
+  <LazyImage src={host.profilePicUrl || DEFAULT_PROFILE_URL} width="5rem" height="5rem" />
 </Fade>;
 
 
