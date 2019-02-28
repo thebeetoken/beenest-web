74a5b2839b4d527ce83feffa1f152312912e2eb2
Vic Woeltjen <vic@thebeetoken.com>
2019-02-27 17:13:41 -0800

    Use a Fade in Listing

--- src/pages/listing/Listing.tsx
+++ src/pages/listing/Listing.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import { Col, Row } from 'reactstrap';
+import { Col, Fade, Row } from 'reactstrap';
 	
 import Footer from 'components/work/Footer';
 import Header from 'components/work/Header';
 
 const Listing = () => (
-  <>
+  <Fade>
     <Header />
     <Row className="w-100">
       <Col>Hello, Listing.</Col>
     </Row>
     <Footer />
-  </>
+  </Fade>
 );
 
 export default Listing;
