ec4b9eeff02b61f628f7c6b4aa8a6c683b24f964
Vic Woeltjen <vic@thebeetoken.com>
2019-02-28 16:37:27 -0800

    Begin adding BookingCard

--- src/pages/listing/BookingCard.tsx
+++ src/pages/listing/BookingCard.tsx
@@ -1,6 +1,30 @@
 import * as React from 'react';
+import { Button, Card, Row } from 'reactstrap';
+
+import DateRangePicker from 'components/work/DateRangePicker';
 import { Listing } from 'networking/listings';
-const BookingCard = (listing: Listing) => (
-  <div>Book {listing.id}</div>
-);
+
+import { formatPrice } from 'utils/formatter';
+
+const BookingCard = ({
+  pricePerNightUsd
+}: Listing) => (<Card>
+  <Row><strong>{formatPrice(pricePerNightUsd)} per night</strong></Row>
+  <Row><DateRangePicker
+    isOutsideRange={() => false}
+    startDate={null} // momentPropTypes.momentObj or null,
+    startDateId="startDate"
+    startDatePlaceholderText="Check-In"
+    daySize={32}
+    endDate={null} // momentPropTypes.momentObj or null,
+    endDateId="endDate"
+    endDatePlaceholderText="Check-Out"
+    onDatesChange={() => console.log('dates change')} // PropTypes.func.isRequired,
+    focusedInput={null} // PropTypes.oneOf(['startDate', 'endDate']) or null,
+    onFocusChange={() => console.log('focus change')} // PropTypes.func.isRequired,
+    minimumNights={1}
+    numberOfMonths={1}
+  /></Row>
+  <Row><Button>Request to Book</Button></Row>
+</Card>);
 export default BookingCard;
--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -36,6 +36,10 @@ export function getGoogleMapURI(listing: Listing): string {
   return `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
 }
 
+export function formatPrice(price: number) { // TODO: Currency?
+  return `\$${price.toFixed(2)}`;
+}
+
 export function formatMonth(date: Date | string) {
   return format(date, 'MMMM YYYY');
 }
