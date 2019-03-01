93d6054b15ba15441796a093e7c4cf6e1352f27c
kevin <kevin@tommy-mbp-2.local>
2019-03-01 14:38:33 -0800

    revert(account): revert account module

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import About from './about';
-import Account from './account';
+import Account from './Account';
 import ForgotPassword from './forgotPassword';
 import Home from './home';
 import Listing from './listing';
