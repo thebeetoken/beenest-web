3eea2b6ad5f9fb8237262d6b411b745c00f7bddb
Vic Woeltjen <vic@thebeetoken.com>
2019-03-01 16:42:59 -0800

    Link from ListingCard to Listing page

--- src/pages/search/ListingCard.tsx
+++ src/pages/search/ListingCard.tsx
@@ -5,24 +5,27 @@ import { ListingShort } from 'networking/listings';
 
 const ListingCard = ({
   homeType,
+  idSlug,
   listingPicUrl,
   pricePerNightUsd,
   title
 }: ListingShort) => (
-  <Card tag={Fade} className="w-100">
-    <div className="embed-responsive embed-responsive-4by3">
-      <div className="embed-responsive-item">
-        <CardImg className="w-100" src={listingPicUrl} alt={`Photo of ${title}`} />
+  <a href={`/work/listings/${idSlug}`} className="w-100 h-100">
+    <Card tag={Fade} className="w-100 h-100">
+      <div className="embed-responsive embed-responsive-4by3">
+        <div className="embed-responsive-item">
+          <CardImg className="w-100" src={listingPicUrl} alt={`Photo of ${title}`} />
+        </div>
       </div>
-    </div>
-    <CardBody>
-      <CardSubtitle>{homeType}</CardSubtitle>
-      <CardTitle tag="h5">{title}</CardTitle>
-    </CardBody>
-    <CardFooter>
-      <CardText>${pricePerNightUsd} per night</CardText>
-    </CardFooter>
-  </Card>
+      <CardBody>
+        <CardSubtitle>{homeType}</CardSubtitle>
+        <CardTitle tag="h5">{title}</CardTitle>
+      </CardBody>
+      <CardFooter>
+        <CardText>${pricePerNightUsd} per night</CardText>
+      </CardFooter>
+    </Card>
+  </a>
 );
 
 export default ListingCard;
