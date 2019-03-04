20ab30b5f72beb06d879d7635bd68682c58860e0
Vic Woeltjen <vic@thebeetoken.com>
2019-03-04 11:37:53 -0800

    Avoid inline style

--- src/pages/listing/ListingGallery.tsx
+++ src/pages/listing/ListingGallery.tsx
@@ -6,7 +6,7 @@ import ImageGrid from 'shared/ImageGrid';
 import { Listing } from 'networking/listings';
 
 const ListingGallery = ({ listingPicUrl, photos }: Listing) => (
-  <Row className="w-100 px-0 mx-0 position-relative" style={{ height: '75vh' }}>
+  <Row className="w-100 height-60vh px-0 mx-0 position-relative">
     <ImageGrid images={[listingPicUrl, ...photos]} />
     <Button className="position-absolute bottom-0 right-0 m-4">
       View Photos <span className="fas fa-camera"></span>
