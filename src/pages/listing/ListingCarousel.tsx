8a1a3aef0dca5411736af2929188a517708b33f2
Vic Woeltjen <vic@thebeetoken.com>
2019-03-04 12:06:19 -0800

    Use Carousel from reactstrap

--- src/pages/listing/ListingCarousel.tsx
+++ src/pages/listing/ListingCarousel.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Slider from 'react-slick';
+import { Carousel, CarouselItem } from 'reactstrap';
 
 import LazyImage from 'components/shared/LazyImage';
 
@@ -10,15 +10,20 @@ interface Props {
 // TODO: add a ref so that keyboard can be used immediately when carousel opens
 // see: https://github.com/akiran/react-slick/issues/738
 
-const ListingCarousel = ({ photos }: Props) =>  (
-  <Slider infinite speed={500} slidesToShow={1} slidesToScroll={1}>
+const ListingCarousel = ({ photos }: Props) =>  {
+  const [index, setIndex] = React.useState<number>(0);
+  const next = () => setIndex(Math.min(index + 1, photos.length - 1));
+  const previous = () => setIndex(Math.max(index - 1, 0));
+  return <Carousel activeIndex={index} next={next} previous={previous}>
     {photos.map((url: string, index: number) => (
-      <figure key={url}>
-        <LazyImage src={url} transition />
-        <figcaption>Photo {index + 1} of {photos.length}</figcaption>
-      </figure>
+      <CarouselItem key={url}>
+        <figure>
+          <LazyImage src={url} transition />
+          <figcaption>Photo {index + 1} of {photos.length}</figcaption>
+        </figure>
+      </CarouselItem>
     ))}
-  </Slider>
-);
+  </Carousel>;
+};
 
 export default ListingCarousel;
