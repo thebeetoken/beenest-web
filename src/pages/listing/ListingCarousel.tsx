7b0a56cc31c101dcd69c6d5a143172d2d460b5de
Vic Woeltjen <vic@thebeetoken.com>
2019-03-04 12:22:56 -0800

    Use UncontrolledCarousel

--- src/pages/listing/ListingCarousel.tsx
+++ src/pages/listing/ListingCarousel.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import { Carousel, CarouselControl, CarouselItem } from 'reactstrap';
-
-import LazyImage from 'components/shared/LazyImage';
+import { UncontrolledCarousel } from 'reactstrap';
 
 interface Props {
   photos: string[];
@@ -10,22 +8,12 @@ interface Props {
 // TODO: add a ref so that keyboard can be used immediately when carousel opens
 // see: https://github.com/akiran/react-slick/issues/738
 
-const ListingCarousel = ({ photos }: Props) =>  {
-  const [index, setIndex] = React.useState<number>(0);
-  const next = () => setIndex(Math.min(index + 1, photos.length - 1));
-  const previous = () => setIndex(Math.max(index - 1, 0));
-  return <Carousel activeIndex={index} next={next} previous={previous}>
-    {photos.map((url: string, index: number) => (
-      <CarouselItem key={url}>
-        <figure>
-          <LazyImage src={url} transition />
-          <figcaption>Photo {index + 1} of {photos.length}</figcaption>
-        </figure>
-      </CarouselItem>
-    ))}
-    <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-    <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
-  </Carousel>;
-};
+const ListingCarousel = ({ photos }: Props) => (
+  <UncontrolledCarousel items={photos.map((url, index) => ({
+    src: url,
+    altText: `Image ${index + 1} of ${photos.length}`,
+    caption: `Image ${index + 1} of ${photos.length}`
+  }))} />
+);
 
 export default ListingCarousel;
