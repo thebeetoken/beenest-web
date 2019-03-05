1ee68c03e86cf349daca970838c37a505819855b
Jeremy Kwa <jeremykwa@gmail.com>
2019-03-04 19:05:57 -0800

    scaffolding

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import ZendeskWebWidgetWrapper from 'HOCs/ZendeskWebWidgetWrapper';
+import Header from 'components/work/Header';
+
 import About from './about';
 import Account from './Account';
 import FirebaseEmailHandler from './Account/FirebaseEmailHandler';
 import ForgotPassword from './forgotPassword';
 import Home from './home';
+import Host from './host';
 import HostSignup from './hosts/signup';
 import Listing from './listing';
 import Login from './login';
@@ -15,10 +19,14 @@ import Search from './search';
 import Signup from './signup';
 import Trips from './trips';
 import AuthenticatedRoute from 'HOCs/AuthenticatedRoute';
-
-import Header from 'components/work/Header';
-
 import '../styled/customStyles.scss';
+
+
+const HostWithZendesk = (props: RouterProps) => (
+  <ZendeskWebWidgetWrapper>
+    <Host {...props} />
+  </ZendeskWebWidgetWrapper>
+);
 
 const Work = () => (
   <div>
@@ -28,6 +36,7 @@ const Work = () => (
       <Route path="/work/account/action" component={FirebaseEmailHandler} />
       <Route path="/work/account" component={Account} />
       <Route path="/work/forgot_password" component={ForgotPassword} />
+      <AuthenticatedRoute path="/work/host" component={HostWithZendesk} />
       <Route exact path="/work/listings/:id" component={Listing} />
       <Route exact path="/work/login" component={Login} />
       <Route exact path="/work/logout" component={Logout} />
---
+++ src/pages/host/Host.tsx
@@ -0,0 +1,102 @@
+import * as React from 'react';
+import { Redirect, Route, Switch, NavLink as RRNavLink } from 'react-router-dom';
+import { Listing, CREATE_LISTING, GET_HOST_LISTINGS } from 'networking/listings';
+import { Container, Fade, Button, NavItem, NavLink, Nav, Row, Col } from 'reactstrap';
+import AuthenticatedRoute from 'HOCs/AuthenticatedRoute';
+import NotFound from '../notFound';
+import { compose, graphql } from 'react-apollo';
+import { getFriendlyErrorMessage } from 'utils/validators';
+
+interface Props extends RouterProps {
+  createListing: () => Promise<Listing>;
+}
+
+const Host = ({ createListing, history }: Props) => {
+  const [isCreateListingClicked, setCreateListingClicked] = React.useState<boolean>(false);
+
+  return (
+    <Container className="pt-8 pb-6" tag={Fade}>
+      <Row>
+        <Col>
+          <h1>Host Profile</h1>
+        </Col>
+        <Col>
+          <Button disabled={isCreateListingClicked} onClick={handleNewListingClick} size="small">
+            Add New Listing
+          </Button>
+        </Col>
+      </Row>
+      <hr />
+      <Nav className="mb-5" tabs>
+        {[
+          {
+          to: '/work/host/bookings',
+            title: 'Bookings',
+          },
+          {
+            to: '/work/host/listings',
+            title: 'Listings',
+          },
+          {
+            to: '/work/host/payments',
+            title: 'Payments',
+          },
+        ].map(({ title, to }) => (
+          <NavItem key={to}>
+            <NavLink 
+              tag={RRNavLink}
+              to={to}>
+              {title}
+            </NavLink>
+          </NavItem>
+        ))}
+      </Nav>
+        <Switch>
+          <AuthenticatedRoute exact path="/work/host/bookings" component={() => <h1>This is Host Bookings</h1>} />
+          <AuthenticatedRoute exact path="/work/host/listings" render={() => <h1>This is Host Listings</h1>} />
+          <AuthenticatedRoute exact path="/work/host/payments" component={() => <h1>This is Host Payments</h1>} />
+          <Redirect from="/work/host" to="/work/host/listings" />
+          <Route component={NotFound} />
+        </Switch>
+    </Container>
+  );
+
+  function handleNewListingClick() {
+    setCreateListingClicked(true);
+    createListing()
+      .then((response: any) => {
+        const { id } = response.data.createListing;
+        history.push(`/work/host/listings/${id}/edit`);
+      })
+      .catch((error: Error) => {
+        console.error(error);
+        setCreateListingClicked(false);
+        alert(`There was a problem creating this listing: ${getFriendlyErrorMessage(error)}`);
+      });
+  }
+}
+
+export default compose(
+  graphql(CREATE_LISTING, {
+    props: ({ mutate }: any) => ({
+      createListing: () => {
+        return mutate({
+          refetchQueries: [{ query: GET_HOST_LISTINGS }],
+          update: (store: any, { data }: any) => {
+            if (!store.data.data.ROOT_QUERY || !store.data.data.ROOT_QUERY.hostListings) {
+              return;
+            }
+            const { createListing } = data;
+            const { hostListings } = store.readQuery({ query: GET_HOST_LISTINGS });
+            store.writeQuery({
+              query: GET_HOST_LISTINGS,
+              data: {
+                hostListings: [ createListing, ...hostListings ],
+              },
+            });
+          },
+        });
+      },
+    }),
+  })
+)(Host);
