c3653ed3d160885cacb0db3485e6f8a7b7d06b24
Vic Woeltjen <vic@thebeetoken.com>
2019-03-05 13:55:29 -0800

    Lay out BookingCard

--- src/pages/listing/BookingCard.tsx
+++ src/pages/listing/BookingCard.tsx
@@ -10,33 +10,41 @@ import { formatPrice } from 'utils/formatter';
 const BookingCard = ({
   pricePerNightUsd
 }: Listing) => (<Card className="p-5 m-3">
-  <Row><strong>{formatPrice(pricePerNightUsd)} per night</strong></Row>
-  <Row><DateRangePicker
-    isOutsideRange={() => false}
-    startDate={null} // momentPropTypes.momentObj or null,
-    startDateId="startDate"
-    startDatePlaceholderText="Check-In"
-    daySize={32}
-    endDate={null} // momentPropTypes.momentObj or null,
-    endDateId="endDate"
-    endDatePlaceholderText="Check-Out"
-    onDatesChange={() => console.log('dates change')} // PropTypes.func.isRequired,
-    focusedInput={null} // PropTypes.oneOf(['startDate', 'endDate']) or null,
-    onFocusChange={() => console.log('focus change')} // PropTypes.func.isRequired,
-    minimumNights={1}
-    numberOfMonths={1}
-  /></Row>
-  <Row><Input
-    type="select"
-    name="numberOfGuests"
-    onChange={() => console.log('Guests changedd')}
-    component="select">
-    {guestsSelectboxOptions.map(option => (
-      <option value={option.value} key={option.value}>
-        {option.option}
-      </option>
-    ))}
-  </Input></Row>
-  <Row><Button>Request to Book</Button></Row>
+  <Row className="m-0">
+    <strong>{formatPrice(pricePerNightUsd)} per night</strong>
+  </Row>
+  <Row className="w-100 m-0">
+    <DateRangePicker
+      isOutsideRange={() => false}
+      startDate={null} // momentPropTypes.momentObj or null,
+      startDateId="startDate"
+      startDatePlaceholderText="Check-In"
+      daySize={32}
+      endDate={null} // momentPropTypes.momentObj or null,
+      endDateId="endDate"
+      endDatePlaceholderText="Check-Out"
+      onDatesChange={() => console.log('dates change')} // PropTypes.func.isRequired,
+      focusedInput={null} // PropTypes.oneOf(['startDate', 'endDate']) or null,
+      onFocusChange={() => console.log('focus change')} // PropTypes.func.isRequired,
+      minimumNights={1}
+      numberOfMonths={1}
+    />
+  </Row>
+  <Row className="w-100 m-0">
+    <Input
+      type="select"
+      name="numberOfGuests"
+      onChange={() => console.log('Guests changedd')}
+      component="select">
+      {guestsSelectboxOptions.map(option => (
+        <option value={option.value} key={option.value}>
+          {option.option}
+        </option>
+      ))}
+    </Input>
+  </Row>
+  <Row className="w-100 m-0">
+    <Button>Request to Book</Button>
+  </Row>
 </Card>);
 export default BookingCard;
