d4a6f2df9b71d52352714fbd08269fbcb9eef73e
kevin <kevin@tommy-mbp-2.local>
2019-03-05 18:20:26 -0800

    fix(comments): address comments

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -13,6 +13,7 @@ import { parseQueryString } from 'utils/queryParams';
 import { VIEWPORT_CENTER_LAYOUT } from 'styled/sharedClasses/layout';
 
 import SearchPage from './SearchPage';
+import { ApolloError } from 'apollo-client';
 
 const SEARCH_PARAMS = [
   'bounds',
@@ -39,54 +40,11 @@ const Search = () => {
             </Container>
           );
         }
-        if (error && error.graphQLErrors[0].extensions && error.graphQLErrors[0].extensions.code === 'BAD_USER_INPUT') {
-          return (
-            <Container tag={Fade} className="d-flex flex-column align-items-center justify-content-center bee-without-header-height-container p-0" fluid>
-              <div
-                className="bg-img-hero d-flex flex-column align-items-center justify-content-center gradient-overlay-half-primary-v1 bee-without-header-height-container w-100"
-                style={{ backgroundImage: `url('https://static.beenest.com/images/app/misc/painted-ladies2.jpg')` }}>
-                <Row className="px-4 mb-4" noGutters>
-                  <h5 className="font-size-md-down-5text-center text-white">Please enter in a city or region to see results.</h5>
-                </Row>
-                <Row className="d-flex justify-content-center px-0 mx-0 bg-white bee-top">
-                  <Col tag={Fade} className="w-100 p-5">
-                    <SearchBar />
-                  </Col>
-                </Row>
-              </div>
-            </Container>
-          );
-        } else if (error) {
-          return (
-            <Container tag={Fade} className={`${VIEWPORT_CENTER_LAYOUT} flex-column bee-without-header-height-container`}>
-              <h2 className="font-size-md-down-5 text-center">{getFriendlyErrorMessage(error)}</h2>
-              <h5 className="text-center">Please contact{' '}
-                <a className="text-primary" href="https://support.beenest.com/" target="_blank">support</a>{' '}
-                if this continues.
-              </h5>
-            </Container>
-          );
-        }
 
+        if (error) isBadUserInputError(error) ? <SearchErrorPage /> : <ErrorPage error={error} />;
+        
         const { searchListings } = data;
-        if (searchListings.length === 0) {
-          return (
-            <Container tag={Fade} className="d-flex flex-column align-items-center justify-content-center bee-without-header-height-container p-0" fluid>
-            <div
-              className="bg-img-hero d-flex flex-column align-items-center justify-content-center gradient-overlay-half-primary-v1 bee-without-header-height-container w-100"
-              style={{ backgroundImage: `url('https://static.beenest.com/images/app/misc/painted-ladies2.jpg')` }}>
-              <Row className="px-4 mb-4" noGutters>
-                <h5 className="text-center text-white font-weight-normal">No listings in this area, please try a different city or region.</h5>
-              </Row>
-              <Row className="d-flex justify-content-center px-0 mx-0 bg-white bee-top">
-                <Col tag={Fade} className="w-100 p-5">
-                  <SearchBar />
-                </Col>
-              </Row>
-            </div>
-          </Container>
-          );
-        }
+        if (searchListings.length < 1) <EmptySearchPage />;
 
         return <SearchPage listings={searchListings} />;
       }}
@@ -94,5 +52,55 @@ const Search = () => {
     <Footer />
   </Fade>);
 };
+
+const SearchErrorPage = () => (
+  <Container tag={Fade} className="d-flex flex-column align-items-center justify-content-center bee-without-header-height-container p-0" fluid>
+    <div
+      className="bg-img-hero d-flex flex-column align-items-center justify-content-center gradient-overlay-half-primary-v1 bee-without-header-height-container w-100"
+      style={{ backgroundImage: `url('https://static.beenest.com/images/app/misc/painted-ladies2.jpg')` }}>
+      <Row className="px-4 mb-4" noGutters>
+        <h5 className="font-size-md-down-5text-center text-white">Please enter in a city or region to see results.</h5>
+      </Row>
+      <Row className="d-flex justify-content-center px-0 mx-0 bg-white bee-top">
+        <Col tag={Fade} className="w-100 p-5">
+          <SearchBar />
+        </Col>
+      </Row>
+    </div>
+  </Container>
+);
+
+interface ErrorPageProps {
+  error: ApolloError
+}
+
+const ErrorPage = (props: ErrorPageProps) => (
+  <Container tag={Fade} className={`${VIEWPORT_CENTER_LAYOUT} flex-column bee-without-header-height-container`}>
+    <h2 className="font-size-md-down-5 text-center">{getFriendlyErrorMessage(props.error)}</h2>
+    <h5 className="text-center">Please contact{' '}
+      <a className="text-primary" href="https://support.beenest.com/" target="_blank">support</a>{' '}
+      if this continues.
+    </h5>
+  </Container>
+);
+
+const EmptySearchPage = () => (
+  <Container tag={Fade} className="d-flex flex-column align-items-center justify-content-center bee-without-header-height-container p-0" fluid>
+    <div
+      className="bg-img-hero d-flex flex-column align-items-center justify-content-center gradient-overlay-half-primary-v1 bee-without-header-height-container w-100"
+      style={{ backgroundImage: `url('https://static.beenest.com/images/app/misc/painted-ladies2.jpg')` }}>
+      <Row className="px-4 mb-4" noGutters>
+        <h5 className="text-center text-white font-weight-normal">No listings in this area, please try a different city or region.</h5>
+      </Row>
+      <Row className="d-flex justify-content-center px-0 mx-0 bg-white bee-top">
+        <Col tag={Fade} className="w-100 p-5">
+          <SearchBar />
+        </Col>
+      </Row>
+    </div>
+  </Container>
+);
+
+const isBadUserInputError = (error: ApolloError) => error && error.graphQLErrors[0].extensions && error.graphQLErrors[0].extensions.code === 'BAD_USER_INPUT';
 
 export default Search;
