56aa37a9c962067ad7ec8e6cf1b05d6a455af541
Tommy Chheng <tommy.chheng@gmail.com>
2019-03-06 15:27:18 -0800

    add more spacing between sections

--- src/pages/listing/ListingInformation.tsx
+++ src/pages/listing/ListingInformation.tsx
@@ -31,62 +31,85 @@ const ListingInformation = ({
 }: Listing) => <Fade className="mt-3">
   <h3>{title}</h3>
   <address>{formatAddress(city, state, country)}</address>
-  <p>Host: {host.displayName}</p>
-  <Avatar user={host} />
-  <h3>Description</h3>
-  <p dangerouslySetInnerHTML={{ __html: sanitizeHtml(description) }} />
-  <h3>Accommodations</h3>
-  <dl className="row">
-    {homeType && <>
-      <dt className="col-6">Home Type</dt>
-      <dd className="col-6">{homeType}</dd>
-    </>}
-    {sleepingArrangement && <>
-      <dt className="col-6">Sleeping arrangement</dt>
-      <dd className="col-6">{sleepingArrangement}</dd>
-    </>}
-    {typeof numberOfBedrooms === 'number' && <>
-      <dt className="col-6">Number of bedrooms</dt>
-      <dd className="col-6">{numberOfBedrooms}</dd>
-    </>}
-    {typeof numberOfBathrooms === 'number' && <>
-      <dt className="col-6">Number of bathrooms</dt>
-      <dd className="col-6">{numberOfBathrooms}</dd>
-    </>}
-    {sharedBathroom && <>
-      <dt className="col-6">Shared bathroom</dt>
-      <dd className="col-6">{sharedBathroom}</dd>
-    </>}
-    {typeof maxGuests === 'number' && <>
-      <dt className="col-6">Maximum number of guests</dt>
-      <dd className="col-6">{maxGuests}</dd>
-    </>}
-    {typeof minimumNights === 'number' && <>
-      <dt className="col-6">Minimum number of nights</dt>
-      <dd className="col-6">{minimumNights}</dd>
-    </>}
-    {checkInTime && <>
-      <dt className="col-6">Check-in</dt>
-      <dd className="col-6">{checkInTime.from} to {checkInTime.to}</dd>
-    </>}
-    {checkOutTime && <>
-      <dt className="col-6">Check-out</dt>
-      <dd className="col-6">{checkOutTime}</dd>
-    </>}
-  </dl>
-  <h3 className={amenities.length > 0 ? '' : 'd-none'}>Amenities</h3>
-  <ul>
-    {amenities.map(amenity => <li key={amenity}>{amenity}</li>)}
-  </ul>
-  <h3 className="d-none d-md-block">Location</h3>
-  <GoogleMaps lat={lat} lng={lng} showCircle />
-  <h3>About {host.displayName}</h3>
-  <p><small>Member since {formatMonth(host.createdAt)}</small></p>
-  <Row className="align-items-center justify-content-between">
-    <Col xs="auto"><Avatar user={host} /></Col>
-    <Col xs="auto"><ContactHostButton listingId={id} host={host} /></Col>
-  </Row>
-  {host.about && <p dangerouslySetInnerHTML={{ __html: sanitizeHtml(host.about) }} />}
+
+  <section className="mb-6">
+    <Avatar user={host} />
+    <p>Host: {host.displayName}</p>
+  </section>
+
+  <section className="mb-6">
+    <h3>Description</h3>
+    <p dangerouslySetInnerHTML={{ __html: sanitizeHtml(description) }} />
+    <hr className="mt-6"/>
+  </section>
+
+  <section className="mb-6">
+    <h3>Accommodations</h3>
+    <dl className="row">
+      {homeType && <>
+        <dt className="col-6">Home Type</dt>
+        <dd className="col-6">{homeType}</dd>
+      </>}
+      {sleepingArrangement && <>
+        <dt className="col-6">Sleeping arrangement</dt>
+        <dd className="col-6">{sleepingArrangement}</dd>
+      </>}
+      {typeof numberOfBedrooms === 'number' && <>
+        <dt className="col-6">Number of bedrooms</dt>
+        <dd className="col-6">{numberOfBedrooms}</dd>
+      </>}
+      {typeof numberOfBathrooms === 'number' && <>
+        <dt className="col-6">Number of bathrooms</dt>
+        <dd className="col-6">{numberOfBathrooms}</dd>
+      </>}
+      {sharedBathroom && <>
+        <dt className="col-6">Shared bathroom</dt>
+        <dd className="col-6">{sharedBathroom}</dd>
+      </>}
+      {typeof maxGuests === 'number' && <>
+        <dt className="col-6">Maximum number of guests</dt>
+        <dd className="col-6">{maxGuests}</dd>
+      </>}
+      {typeof minimumNights === 'number' && <>
+        <dt className="col-6">Minimum number of nights</dt>
+        <dd className="col-6">{minimumNights}</dd>
+      </>}
+      {checkInTime && <>
+        <dt className="col-6">Check-in</dt>
+        <dd className="col-6">{checkInTime.from} to {checkInTime.to}</dd>
+      </>}
+      {checkOutTime && <>
+        <dt className="col-6">Check-out</dt>
+        <dd className="col-6">{checkOutTime}</dd>
+      </>}
+    </dl>
+    <hr className="mt-6"/>
+  </section>
+
+  <section className="mb-6">
+    <h3 className={amenities.length > 0 ? '' : 'd-none'}>Amenities</h3>
+    <ul>
+      {amenities.map(amenity => <li key={amenity}>{amenity}</li>)}
+    </ul>
+    <hr className="mt-6"/>
+  </section>
+
+  <section className="mb-6">
+    <h3 className="d-none d-md-block">Location</h3>
+    <GoogleMaps lat={lat} lng={lng} showCircle />
+    <hr className="mt-6"/>
+  </section>
+
+  <section className="mb-6">
+    <h3>About {host.displayName}</h3>
+    <p><small>Member since {formatMonth(host.createdAt)}</small></p>
+    <Row className="align-items-center justify-content-between">
+      <Col xs="auto"><Avatar user={host} /></Col>
+      <Col xs="auto"><ContactHostButton listingId={id} host={host} /></Col>
+    </Row>
+    {host.about && <p dangerouslySetInnerHTML={{ __html: sanitizeHtml(host.about) }} />}
+  </section>
+
 </Fade>;
 
 export default ListingInformation;
