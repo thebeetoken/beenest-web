a3c785a8d8045d8f9add85fb2b190775806f576b
kevin <kevin@tommy-mbp-2.local>
2019-03-07 13:23:37 -0800

    fix(styles): fix styles

--- src/pages/bookings/Booking.tsx
+++ src/pages/bookings/Booking.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Fade } from 'reactstrap';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import BookingContainer from 'legacy/routes/Booking/Booking.container';
@@ -7,17 +8,19 @@ import BookingPayment from 'legacy/routes/Booking/BookingPayment';
 import BookingReceipt from 'legacy/routes/Booking/BookingReceipt';
 
 const Booking = () => (
-  <BookingContainer className="bee-booking">
-    <div className="booking-body-container">
-      <Switch>
-        <Route exact path="/bookings/:id/options" component={BookingOptions} />
-        <Route exact path="/bookings/:id/payment" component={BookingPayment} />
-        <Route exact path="/bookings/:id/receipt" component={BookingReceipt} />
-        <Redirect from="/bookings/:id" to="/bookings/:id/options" />
-        <Route component={() => <Redirect to="/" />} />
-      </Switch>
-    </div>
-  </BookingContainer>
+  <Fade>
+    <BookingContainer className="bee-booking">
+      <div className="booking-body-container">
+        <Switch>
+          <Route exact path="/bookings/:id/options" component={BookingOptions} />
+          <Route exact path="/bookings/:id/payment" component={BookingPayment} />
+          <Route exact path="/bookings/:id/receipt" component={BookingReceipt} />
+          <Redirect from="/bookings/:id" to="/bookings/:id/options" />
+          <Route component={() => <Redirect to="/" />} />
+        </Switch>
+      </div>
+    </BookingContainer>
+  </Fade>
 );
 
 export default Booking;
