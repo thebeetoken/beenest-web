c28b120150b19b6aebd62fc441cc35139b7641ff
Vic Woeltjen <vic@thebeetoken.com>
2019-03-07 14:03:23 -0800

    Allow chosen place to be cleared

--- src/pages/search/filters/TransitTime.tsx
+++ src/pages/search/filters/TransitTime.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Container, Input } from 'reactstrap';
+import { Button, Container, Input, Row } from 'reactstrap';
 
 import GoogleAutoComplete from 'components/shared/GoogleAutoComplete';
 
@@ -9,6 +9,7 @@ const TransitTime = () => {
   return <Container>
     <h5 className={place ? '' : 'text-muted'}>
       {place ? place.name : 'No place specified'}
+      <Button close className={place ? 'mt-2' : 'd-none'} onClick={() => setPlace(null)} />
     </h5>
     <GoogleAutoComplete
       types={[]}
