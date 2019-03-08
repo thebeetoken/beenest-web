075ab20d195840f6c259a39f7a77f94af67dd7b2
Vic Woeltjen <vic@thebeetoken.com>
2019-03-08 15:47:49 -0800

    Show custom markers

--- src/legacy/shared/GoogleMapsWithMarkers/GoogleMapsWithMarkers.tsx
+++ src/legacy/shared/GoogleMapsWithMarkers/GoogleMapsWithMarkers.tsx
@@ -11,6 +11,9 @@ import { LatLngBounds, ListingShort } from 'networking/listings';
 
 import GoogleMapsWithMarkersContainer from './GoogleMapsWithMarkers.container';
 
+const hotelMarker = require('assets/images/iconmonstr-location-12-32.png');
+const nearMarker = require('assets/images/iconmonstr-location-13-32.png');
+
 interface Props extends RouterProps {
   bounds?: LatLngBounds;
   children?: React.ReactNode;
@@ -62,15 +65,7 @@ class GoogleMapsWithMarkers extends React.Component<Props, State> {
         ref={this.handleMapMounted}
       >
         {near && <Marker
-          icon={{
-            fillColor: '#AADBFF',
-            fillOpacity: 1,
-            labelOrigin: { x: 0, y: -5 },
-            scale: 4,
-            strokeColor: '#1164FF',
-            path: google.maps.SymbolPath.CIRCLE,
-            strokeWeight: 2
-          }}
+          icon={nearMarker}
           label={{
             color: '#333',
             fontSize: '1rem',
@@ -82,8 +77,10 @@ class GoogleMapsWithMarkers extends React.Component<Props, State> {
         />}
         {listings.map(listing => (
           <Marker key={listing.id}
+            icon={hotelMarker}
             position={{ lat: listing.lat, lng: listing.lng }}
-            onClick={() => this.setState({ selectedListing: listing })} />
+            onClick={() => this.setState({ selectedListing: listing })}
+          />
         ))}
         {!!selectedListing && <InfoWindow
           position={{ lat: selectedListing.lat, lng: selectedListing.lng }}
