d70f9702cb1c96fabbcf88f752ed61ba9053f8f1
Vic Woeltjen <vic@thebeetoken.com>
2019-03-08 17:33:42 -0800

    Move useDebounce to utils

---
+++ src/utils/hooks.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+// https://dev.to/gabe_ragland/debouncing-with-react-hooks-jci
+export function useDebounce(value: any, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value]);
+
+  return debouncedValue;
+};
