aaba992928f689c2d901b2490ec0fe396ab677e1
kevin <kevin@tommy-mbp-2.local>
2019-03-11 15:25:51 -0700

    feat(share): made entire row clickable for ease of use

--- src/pages/listing/ListingGallery.tsx
+++ src/pages/listing/ListingGallery.tsx
@@ -8,7 +8,7 @@ import CarouselPortal from 'legacy/work/CarouselPortal';
 
 import { Listing } from 'networking/listings';
 
-interface Props extends RouterProps, Listing  { };
+interface Props extends RouterProps, Listing { };
 
 const ListingGallery = (props: Props) => {
   const [isOpen, setOpen] = React.useState<boolean>(false);
@@ -25,40 +25,42 @@ const ListingGallery = (props: Props) => {
       <ModalHeader className="text-primary" toggle={() => toggleModal()}>Share Listing</ModalHeader>
       <ModalBody>
         <Row className="d-flex align-items-center" noGutters>
-          <Col xs="2" md="1">
-            <a href={`https://www.facebook.com/sharer/sharer.php?u=${listingLink}&quote=${title}`} target="_blank">
+          <a className="d-flex align-items-center w-100" href={`https://www.facebook.com/sharer/sharer.php?u=${listingLink}&quote=${title}`} target="_blank">
+            <Col xs="2" md="1" className="p-0">
               <Button className="btn btn-icon transition-3d-hover btn-facebook" size="sm" type="button">
                 <span className="fab btn-icon__inner fab fa-facebook-f" />
               </Button>
-            </a>
-          </Col>
-          <Col className="ml-lg-2">
-            <h5 className="small mb-0">Facebook</h5>
-          </Col>
+            </Col>
+            <Col className="">
+              <h5 className="small mb-0 text-secondary">Facebook</h5>
+            </Col>
+          </a>
         </Row>
         <Row className="d-flex align-items-center mt-3" noGutters>
-          <Col xs="2" md="1">
-            <a href={`https://twitter.com/intent/tweet?url=${listingLink}&text=${title}`} target="_blank">
+          <a className="d-flex align-items-center w-100" href={`https://twitter.com/intent/tweet?url=${listingLink}&text=${title}`} target="_blank">
+            <Col xs="2" md="1" className="p-0">
               <Button className="btn btn-icon transition-3d-hover btn-twitter" size="sm" type="button">
                 <span className="fab btn-icon__inner fab fa-twitter" />
               </Button>
-            </a>
-          </Col>
-          <Col className="ml-lg-2">
-            <h5 className="small mb-0">Twitter</h5>
-          </Col>
+            </Col>
+            <Col className="">
+              <h5 className="small mb-0 text-secondary">Twitter</h5>
+            </Col>
+          </a>
         </Row>
         <Row className="d-flex align-items-center mt-3" noGutters>
-          <Col xs="2" md="1">
-            <CopyToClipboard text={listingLink}>
-              <Button className="btn btn-icon transition-3d-hover" size="sm" type="button">
-                <span className="fab btn-icon__inner far fa-copy" />
-              </Button>
-            </CopyToClipboard>
-          </Col>
-          <Col className="ml-lg-2">
-            <h5 className="small mb-0">Copy Link</h5>
-          </Col>
+          <CopyToClipboard text={listingLink}>
+            <div className="d-flex align-items-center w-100 pointer">
+              <Col xs="2" md="1" className="p-0">
+                <Button className="btn btn-icon transition-3d-hover" size="sm" type="button">
+                  <span className="fab btn-icon__inner far fa-copy" />
+                </Button>
+              </Col>
+              <Col className="">
+                <h5 className="small mb-0 text-secondary">Copy Link</h5>
+              </Col>
+            </div>
+          </CopyToClipboard>
         </Row>
       </ModalBody>
     </Modal>
