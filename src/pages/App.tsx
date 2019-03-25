b019b9cbfacda358784dabdfa9dbd95b35182b93
kevin <kevin@tommy-mbp-2.local>
2019-03-24 21:59:56 -0700

    fix(import): fix import

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -8,7 +8,7 @@ import Footer from 'legacy/work/Footer';
 import About from './about';
 import Account from './account';
 import Booking from 'legacy/routes/Booking';
-import FirebaseEmailHandler from './Account/FirebaseEmailHandler';
+import FirebaseEmailHandler from './account/FirebaseEmailHandler';
 import ForgotPassword from './forgotPassword';
 import Home from './home';
 import Host from 'legacy/routes/Host';
